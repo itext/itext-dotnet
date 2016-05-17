0ac0e5fb933e6717bf3f62772933d79abfbb0122
Alexey Subach <alexey.subach@duallab.com>
2016-05-17 11:32:54 +0300

    Add log4net logging checks
    DEVSIX-630

--- itextsharp.pdftest/itextsharp/test/ITextMemoryAppender.cs
+++ itextsharp.pdftest/itextsharp/test/ITextMemoryAppender.cs
@@ -3,7 +3,7 @@ using log4net.Appender;
 using log4net.Core;
 
 namespace iTextSharp.Test {
-    public class ItextMemoryAppender : MemoryAppender {
+    public class ITextMemoryAppender : MemoryAppender {
         protected override void Append(LoggingEvent le) {
             Console.WriteLine(le.LoggerName + ": " + le.RenderedMessage);
             base.Append(le);
