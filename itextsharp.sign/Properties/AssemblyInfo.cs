a24c9e1224872a568165bfff9430faf2680bc800
Yulian Gaponenko <yulian.gaponenko@duallab.com>
2016-05-19 16:00:00 +0300

    Reorganize sign module
    DEVSIX-634

--- itextsharp.sign/Properties/AssemblyInfo.cs
+++ itextsharp.sign/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("sign")]
+[assembly: AssemblyTitle("itextsharp.sign")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("sign")]
+[assembly: AssemblyProduct("itextsharp.sign")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
