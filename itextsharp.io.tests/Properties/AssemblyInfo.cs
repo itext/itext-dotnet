c2c7486b86a1e809dbe6bf213db682b4cb851784
Alexey Subach <alexey.subach@duallab.com>
2016-05-25 11:24:07 +0300

    Add itextsharp prefix to assembly names

--- itextsharp.io.tests/Properties/AssemblyInfo.cs
+++ itextsharp.io.tests/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("io.tests")]
+[assembly: AssemblyTitle("itextsharp.io.tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
