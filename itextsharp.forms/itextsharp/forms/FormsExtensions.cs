3080e6d6a2e7fa5664240b57e92bc0a411f6024f
Alexey Subach <alexey.subach@duallab.com>
2016-05-31 17:16:05 +0300

    Forms extensions. Remove unused references
    DEVSIX-641

--- itextsharp.forms/itextsharp/forms/FormsExtensions.cs
+++ itextsharp.forms/itextsharp/forms/FormsExtensions.cs
@@ -1,25 +1,26 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
-namespace com.itextpdf.forms
-{
+namespace iTextSharp.Forms {
     internal static class FormsExtensions {
-        public static void AddAll<T>(this ICollection<T> t,  IEnumerable<T> newItems)
-        {
-            foreach (T item in newItems)
-            {
-                    t.Add(item);
+        public static void AddAll<T>(this ICollection<T> t, IEnumerable<T> newItems) {
+            foreach (T item in newItems) {
+                t.Add(item);
             }
         }
 
-        public static String JSubstring(this String str, int beginIndex, int endIndex)
-        {
+        public static String JSubstring(this String str, int beginIndex, int endIndex) {
             return str.Substring(beginIndex, endIndex - beginIndex);
         }
 
-        public static byte[] GetBytes(this String str)
-        {
+        public static byte[] GetBytes(this String str) {
             return System.Text.Encoding.UTF8.GetBytes(str);
+        }
+
+        public static T[] ToArray<T>(this ICollection<T> col, T[] toArray) {
+            T[] r = col.ToArray();
+            return r;
         }
     }
 }
