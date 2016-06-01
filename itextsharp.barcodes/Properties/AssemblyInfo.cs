3aea5ab15730e87b3364f9f999c5f38053f2b875
Alexey Subach <alexey.subach@duallab.com>
2016-06-01 14:36:53 +0300

    Fix assembly infos: update assembly names, add visibility of internals for tests

--- itextsharp.barcodes/Properties/AssemblyInfo.cs
+++ itextsharp.barcodes/Properties/AssemblyInfo.cs
@@ -5,14 +5,15 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("bacodes")]
+[assembly: AssemblyTitle("barcodes")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("bacodes")]
+[assembly: AssemblyProduct("barcodes")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: InternalsVisibleTo("itextsharp.barcodes.tests")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
--- itextsharp.forms/Properties/AssemblyInfo.cs
+++ itextsharp.forms/Properties/AssemblyInfo.cs
@@ -5,14 +5,15 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("forms")]
+[assembly: AssemblyTitle("itextsharp.forms")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("forms")]
+[assembly: AssemblyProduct("itextsharp.forms")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: InternalsVisibleTo("itextsharp.forms.tests")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
