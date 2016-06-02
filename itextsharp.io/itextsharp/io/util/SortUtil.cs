8031bbfa40a6cc929b4b7ba33698fc5fba724186
Pavel Morozov <pavel.morozov@duallab.com>
2016-06-02 19:39:12 +0300

    mergesort for arrays

--- itextsharp.io/itextsharp/io/util/SortUtil.cs
+++ itextsharp.io/itextsharp/io/util/SortUtil.cs
@@ -71,5 +71,58 @@ namespace iTextSharp.IO.Util {
                 list[right--] = temp[i];
             }
         }
+
+        public static void MergeSort<T>(T[] array, Comparison<T> comparison)
+        {
+            MergeSort(array, 0, array.Length - 1, comparison);
+        }
+
+        public static void MergeSort<T>(T[] array) 
+        {
+            MergeSort(array, delegate(T o1, T o2) { return Comparer<T>.Default.Compare(o1, o2); });
+        }
+
+        public static void MergeSort(String[] array) 
+        {
+            MergeSort(array, delegate(String o1, String o2) { return String.CompareOrdinal(o1, o2); });
+        }
+
+        private static void MergeSort<T>(T[] array, int left, int right, Comparison<T> comparison)
+        {
+            if (right > left)
+            {
+                int mid = (right + left) / 2;
+                MergeSort(array, left, mid, comparison);
+                MergeSort(array, (mid + 1), right, comparison);
+
+                Merge(array, left, (mid + 1), right, comparison);
+            }
+        }
+
+        private static void Merge<T>(T[] array, int left, int mid, int right, Comparison<T> comparison)
+        {
+            int eol = (mid - 1), num = (right - left + 1);
+
+            List<T> temp = new List<T>(num);
+
+            while ((left <= eol) && (mid <= right))
+            {
+                if (comparison(array[left], array[mid]) <= 0)
+                    temp.Add(array[left++]);
+                else
+                    temp.Add(array[mid++]);
+            }
+
+            while (left <= eol)
+                temp.Add(array[left++]);
+
+            while (mid <= right)
+                temp.Add(array[mid++]);
+
+            for (int i = num - 1; i >= 0; i--)
+            {
+                array[right--] = temp[i];
+            }
+        }
     }
 }
