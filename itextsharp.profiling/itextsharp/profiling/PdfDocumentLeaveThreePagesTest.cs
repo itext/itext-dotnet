ac463b0cd4a819eefd049803459ac9f692c31080
Alexey Subach <alexey.subach@duallab.com>
2016-06-08 20:15:50 +0300

    Ignore a profiling test for now. It takes too much time to run

--- itextsharp.profiling/itextsharp/profiling/PdfDocumentLeaveThreePagesTest.cs
+++ itextsharp.profiling/itextsharp/profiling/PdfDocumentLeaveThreePagesTest.cs
@@ -10,7 +10,7 @@ namespace iTextSharp.Profiling
             .TestDirectory + "/../../resources/itextsharp/profiling/PdfDocumentTest/";
 
         [Test]
-        [Timeout(300000)]
+        [Ignore("")]
         public void Test() {
             ChangeMediaBox(sourceFolder + "100000PagesDocument.pdf", false, 1.90f);
         }
