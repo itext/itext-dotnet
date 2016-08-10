e368bc4b6f1d5c5f264b4babf94124c04168c449
Yulian Gaponenko <yulian.gaponenko@duallab.com>
2016-08-10 18:20:39 +0300

    Add new tests for encryption with public key certificate
    DEVSIX-576
    
    Autoported commit.
    Original commit hash: [a79e6c13f6]

---
+++ itext/itext.kernel/itext/kernel/crypto/CryptoUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Pkcs;
+using Org.BouncyCastle.X509;
+
+namespace iText.Kernel.Crypto {
+    /// <summary>
+    /// This file is a helper class for internal usage only.
+    /// Be aware that it's API and functionality may be changed in future.
+    /// </summary>
+    public static class CryptoUtil {
+
+        public static X509Certificate ReadPublicCertificate(Stream s) {
+            return new X509CertificateParser().ReadCertificate(s);
+        }
+
+        /// <exception cref="Org.BouncyCastle.Security.GeneralSecurityException"/>
+        /// <exception cref="System.IO.IOException"/>
+        public static ICipherParameters ReadPrivateKeyFromPkcs12KeyStore(Stream keyStore, String pkAlias, char[] pkPassword) {
+            return new Pkcs12Store(keyStore, pkPassword).GetKey(pkAlias).Key;
+        }
+
+    }
+}
