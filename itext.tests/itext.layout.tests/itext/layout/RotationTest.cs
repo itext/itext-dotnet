224ca94fe191ebbc8dc1bd233c7d44355517b54a
Vladimir Osipchuk <vladimir.osipchuk@duallab.com>
2016-08-23 19:11:57 +0300

    Add a new rotation sample
    Autoported commit.
    Original commit hash: [89df54b46b]

--- itext.tests/itext.layout.tests/itext/layout/RotationTest.cs
+++ itext.tests/itext.layout.tests/itext/layout/RotationTest.cs
@@ -291,6 +291,23 @@ namespace iText.Layout {
         /// <exception cref="System.IO.IOException"/>
         /// <exception cref="System.Exception"/>
         [NUnit.Framework.Test]
+        [NUnit.Framework.Ignore("fix background rotation")]
+        public virtual void CellRotationTest01() {
+            String outFileName = destinationFolder + "cellRotationTest01.pdf";
+            String cmpFileName = sourceFolder + cmpPrefix + "cellRotationTest01.pdf";
+            PdfDocument pdfDoc = new PdfDocument(new PdfWriter(outFileName));
+            Document doc = new Document(pdfDoc);
+            Table table = new Table(1).SetWidth(50).AddCell(new Cell().Add(new Paragraph("Helloo")).SetRotationAngle(Math
+                .PI * 70 / 180.0).SetBackgroundColor(Color.GREEN));
+            doc.Add(table);
+            doc.Close();
+            NUnit.Framework.Assert.IsNull(new CompareTool().CompareByContent(outFileName, cmpFileName, destinationFolder
+                , "diff"));
+        }
+
+        /// <exception cref="System.IO.IOException"/>
+        /// <exception cref="System.Exception"/>
+        [NUnit.Framework.Test]
         public virtual void DivRotationTest01() {
             String outFileName = destinationFolder + "divRotationTest01.pdf";
             String cmpFileName = sourceFolder + cmpPrefix + "divRotationTest01.pdf";
