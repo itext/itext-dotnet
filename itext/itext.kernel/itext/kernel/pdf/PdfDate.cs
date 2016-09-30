141dc0f21c34a4b9f2ddcf45dcd2f17ecf40c5d1
Yulian Gaponenko <yulian.gaponenko@duallab.com>
2016-09-30 14:10:43 +0300

    Document PdfTrapNetworkAnnotation class
    DEVSIX-742
    
    Autoported commit.
    Original commit hash: [16c4f32985]

--- itext/itext.kernel/itext/kernel/pdf/PdfDate.cs
+++ itext/itext.kernel/itext/kernel/pdf/PdfDate.cs
@@ -46,204 +46,184 @@ using System.Text;
 using System.Globalization;
 
 
-namespace iText.Kernel.Pdf
-{
-	/// <summary>
-	/// <c>PdfDate</c>
-	/// is the PDF date object.
-	/// <P>
-	/// PDF defines a standard date format. The PDF date format closely follows the format
-	/// defined by the international standard ASN.1 (Abstract Syntax Notation One, defined
-	/// in CCITT X.208 or ISO/IEC 8824). A date is a
-	/// <c>PdfString</c>
-	/// of the form:
-	/// <P><BLOCKQUOTE>
-	/// (D:YYYYMMDDHHmmSSOHH'mm')
-	/// </BLOCKQUOTE><P>
-	/// This object is described in the 'Portable Document Format Reference Manual version 1.3'
-	/// section 7.2 (page 183-184)
-	/// </summary>
-	/// <seealso cref="PdfString"/>
-	/// <seealso cref="Java.Util.GregorianCalendar"/>
-	public class PdfDate : PdfObjectWrapper<PdfString>
-	{
-		/// <summary>
-		/// Constructs a
-		/// <c>PdfDate</c>
-		/// -object.
-		/// </summary>
-		/// <param name="d">
-		/// the date that has to be turned into a
-		/// <c>PdfDate</c>
-		/// &gt;-object
-		/// </param>
-		public PdfDate(DateTime d)
-			: base(new PdfString(GenerateStringByDateTime(d)))
-		{
-		}
+namespace iText.Kernel.Pdf {
+    /// <summary>
+    /// <c>PdfDate</c>
+    /// is the PDF date object.
+    /// <P>
+    /// PDF defines a standard date format. The PDF date format closely follows the format
+    /// defined by the international standard ASN.1 (Abstract Syntax Notation One, defined
+    /// in CCITT X.208 or ISO/IEC 8824). A date is a
+    /// <c>PdfString</c>
+    /// of the form:
+    /// <P><BLOCKQUOTE>
+    /// (D:YYYYMMDDHHmmSSOHH'mm')
+    /// </BLOCKQUOTE><P>
+    /// See also ISO-320001 7.9.4, “Dates”.
+    /// </summary>
+    /// <seealso cref="PdfString"/>
+    /// <seealso cref="Java.Util.GregorianCalendar"/>
+    public class PdfDate : PdfObjectWrapper<PdfString> {
+        /// <summary>
+        /// Constructs a
+        /// <c>PdfDate</c>
+        /// -object.
+        /// </summary>
+        /// <param name="d">
+        /// the date that has to be turned into a
+        /// <c>PdfDate</c>
+        /// &gt;-object
+        /// </param>
+        public PdfDate(DateTime d)
+            : base(new PdfString(GenerateStringByDateTime(d))) {
+        }
 
-		/// <summary>
-		/// Constructs a
-		/// <c>PdfDate</c>
-		/// -object, representing the current day and time.
-		/// </summary>
-		public PdfDate()
-			: this(DateTime.Now)
-		{
-		}
+        /// <summary>
+        /// Constructs a
+        /// <c>PdfDate</c>
+        /// -object, representing the current day and time.
+        /// </summary>
+        public PdfDate()
+            : this(DateTime.Now) {
+        }
 
-		/// <summary>Gives the W3C format of the PdfDate.</summary>
-		/// <returns>a formatted date</returns>
-		public virtual String GetW3CDate()
-		{
-			return GetW3CDate(GetPdfObject().GetValue());
-		}
+        /// <summary>Gives the W3C format of the PdfDate.</summary>
+        /// <returns>a formatted date</returns>
+        public virtual String GetW3CDate() {
+            return GetW3CDate(GetPdfObject().GetValue());
+        }
 
-		/// <summary>
-		/// Gives the W3C format of the
-		/// <c>PdfDate</c>
-		/// .
-		/// </summary>
-		/// <param name="d">the date in the format D:YYYYMMDDHHmmSSOHH'mm'</param>
-		/// <returns>a formatted date</returns>
-		public static String GetW3CDate(String d)
-		{
-			if (d.StartsWith("D:"))
-			{
-				d = d.Substring(2);
-			}
-			StringBuilder sb = new StringBuilder();
-			if (d.Length < 4)
-			{
-				return "0000";
-			}
-			sb.Append(d.JSubstring(0, 4));
-			//year
-			d = d.Substring(4);
-			if (d.Length < 2)
-			{
-				return sb.ToString();
-			}
-			sb.Append('-').Append(d.JSubstring(0, 2));
-			//month
-			d = d.Substring(2);
-			if (d.Length < 2)
-			{
-				return sb.ToString();
-			}
-			sb.Append('-').Append(d.JSubstring(0, 2));
-			//day
-			d = d.Substring(2);
-			if (d.Length < 2)
-			{
-				return sb.ToString();
-			}
-			sb.Append('T').Append(d.JSubstring(0, 2));
-			//hour
-			d = d.Substring(2);
-			if (d.Length < 2)
-			{
-				sb.Append(":00Z");
-				return sb.ToString();
-			}
-			sb.Append(':').Append(d.JSubstring(0, 2));
-			//minute
-			d = d.Substring(2);
-			if (d.Length < 2)
-			{
-				sb.Append('Z');
-				return sb.ToString();
-			}
-			sb.Append(':').Append(d.JSubstring(0, 2));
-			//second
-			d = d.Substring(2);
-			if (d.StartsWith("-") || d.StartsWith("+"))
-			{
-				String sign = d.JSubstring(0, 1);
-				d = d.Substring(1);
-				if (d.Length >= 2)
-				{
-					String h = d.JSubstring(0, 2);
-					String m = "00";
-					if (d.Length > 2)
-					{
-						d = d.Substring(3);
-						if (d.Length >= 2)
-						{
-							m = d.JSubstring(0, 2);
-						}
-					}
-					sb.Append(sign).Append(h).Append(':').Append(m);
-					return sb.ToString();
-				}
-			}
-			sb.Append('Z');
-			return sb.ToString();
-		}
+        /// <summary>
+        /// Gives the W3C format of the
+        /// <c>PdfDate</c>
+        /// .
+        /// </summary>
+        /// <param name="d">the date in the format D:YYYYMMDDHHmmSSOHH'mm'</param>
+        /// <returns>a formatted date</returns>
+        public static String GetW3CDate(String d) {
+            if (d.StartsWith("D:")) {
+                d = d.Substring(2);
+            }
+            StringBuilder sb = new StringBuilder();
+            if (d.Length < 4) {
+                return "0000";
+            }
+            sb.Append(d.JSubstring(0, 4));
+            //year
+            d = d.Substring(4);
+            if (d.Length < 2) {
+                return sb.ToString();
+            }
+            sb.Append('-').Append(d.JSubstring(0, 2));
+            //month
+            d = d.Substring(2);
+            if (d.Length < 2) {
+                return sb.ToString();
+            }
+            sb.Append('-').Append(d.JSubstring(0, 2));
+            //day
+            d = d.Substring(2);
+            if (d.Length < 2) {
+                return sb.ToString();
+            }
+            sb.Append('T').Append(d.JSubstring(0, 2));
+            //hour
+            d = d.Substring(2);
+            if (d.Length < 2) {
+                sb.Append(":00Z");
+                return sb.ToString();
+            }
+            sb.Append(':').Append(d.JSubstring(0, 2));
+            //minute
+            d = d.Substring(2);
+            if (d.Length < 2) {
+                sb.Append('Z');
+                return sb.ToString();
+            }
+            sb.Append(':').Append(d.JSubstring(0, 2));
+            //second
+            d = d.Substring(2);
+            if (d.StartsWith("-") || d.StartsWith("+")) {
+                String sign = d.JSubstring(0, 1);
+                d = d.Substring(1);
+                if (d.Length >= 2) {
+                    String h = d.JSubstring(0, 2);
+                    String m = "00";
+                    if (d.Length > 2) {
+                        d = d.Substring(3);
+                        if (d.Length >= 2) {
+                            m = d.JSubstring(0, 2);
+                        }
+                    }
+                    sb.Append(sign).Append(h).Append(':').Append(m);
+                    return sb.ToString();
+                }
+            }
+            sb.Append('Z');
+            return sb.ToString();
+        }
 
-		/// <summary>
-		/// Converts a PDF string representing a date into a
-		/// <c>DateTime</c>
-		/// .
-		/// </summary>
-		/// <param name="s">the PDF string representing a date</param>
-		/// <returns>
-		/// a
-		/// <c>DateTime</c>
-		/// representing the date
-		/// </returns>
-		public static DateTime Decode(String s) {
-			if (s.StartsWith("D:"))
-				s = s.Substring(2);
-			int year, month = 1, day = 1, hour = 0, minute = 0, second = 0;
-			int offsetHour = 0, offsetMinute = 0;
-			char variation = '\0';
-			year = int.Parse(s.Substring(0, 4));
-			if (s.Length >= 6) {
-				month = int.Parse(s.Substring(4, 2));
-				if (s.Length >= 8) {
-					day = int.Parse(s.Substring(6, 2));
-					if (s.Length >= 10) {
-						hour = int.Parse(s.Substring(8, 2));
-						if (s.Length >= 12) {
-							minute = int.Parse(s.Substring(10, 2));
-							if (s.Length >= 14) {
-								second = int.Parse(s.Substring(12, 2));
-							}
-						}
-					}
-				}
-			}
-			DateTime d = new DateTime(year, month, day, hour, minute, second);
-			if (s.Length <= 14)
-				return d;
-			variation = s[14];
-			if (variation == 'Z')
-				return d.ToLocalTime();
-			if (s.Length >= 17) {
-				offsetHour = int.Parse(s.Substring(15, 2));
-				if (s.Length >= 20) {
-					offsetMinute = int.Parse(s.Substring(18, 2));
-				}
-			}
-			TimeSpan span = new TimeSpan(offsetHour, offsetMinute, 0);
-			if (variation == '-')
-				d += span;
-			else
-				d -= span;
-			return d.ToLocalTime();
-		}
+        /// <summary>
+        /// Converts a PDF string representing a date into a
+        /// <c>DateTime</c>
+        /// .
+        /// </summary>
+        /// <param name="s">the PDF string representing a date</param>
+        /// <returns>
+        /// a
+        /// <c>DateTime</c>
+        /// representing the date
+        /// </returns>
+        public static DateTime Decode(String s) {
+            if (s.StartsWith("D:"))
+                s = s.Substring(2);
+            int year, month = 1, day = 1, hour = 0, minute = 0, second = 0;
+            int offsetHour = 0, offsetMinute = 0;
+            char variation = '\0';
+            year = int.Parse(s.Substring(0, 4));
+            if (s.Length >= 6) {
+                month = int.Parse(s.Substring(4, 2));
+                if (s.Length >= 8) {
+                    day = int.Parse(s.Substring(6, 2));
+                    if (s.Length >= 10) {
+                        hour = int.Parse(s.Substring(8, 2));
+                        if (s.Length >= 12) {
+                            minute = int.Parse(s.Substring(10, 2));
+                            if (s.Length >= 14) {
+                                second = int.Parse(s.Substring(12, 2));
+                            }
+                        }
+                    }
+                }
+            }
+            DateTime d = new DateTime(year, month, day, hour, minute, second);
+            if (s.Length <= 14)
+                return d;
+            variation = s[14];
+            if (variation == 'Z')
+                return d.ToLocalTime();
+            if (s.Length >= 17) {
+                offsetHour = int.Parse(s.Substring(15, 2));
+                if (s.Length >= 20) {
+                    offsetMinute = int.Parse(s.Substring(18, 2));
+                }
+            }
+            TimeSpan span = new TimeSpan(offsetHour, offsetMinute, 0);
+            if (variation == '-')
+                d += span;
+            else
+                d -= span;
+            return d.ToLocalTime();
+        }
 
-		protected internal override bool IsWrappedObjectMustBeIndirect()
-		{
-			return false;
-		}
+        protected internal override bool IsWrappedObjectMustBeIndirect() {
+            return false;
+        }
 
-		private static String GenerateStringByDateTime(DateTime d)
-		{
-			String value = d.ToString("\\D\\:yyyyMMddHHmmss", DateTimeFormatInfo.InvariantInfo);
-			String timezone = d.ToString("zzz", DateTimeFormatInfo.InvariantInfo).Replace(":", "'") + "'";
-			return value + timezone;
-		}
-	}
+        private static String GenerateStringByDateTime(DateTime d) {
+            String value = d.ToString("\\D\\:yyyyMMddHHmmss", DateTimeFormatInfo.InvariantInfo);
+            String timezone = d.ToString("zzz", DateTimeFormatInfo.InvariantInfo).Replace(":", "'") + "'";
+            return value + timezone;
+        }
+    }
 }
--- itext/itext.kernel/itext/kernel/pdf/annot/PdfTrapNetworkAnnotation.cs
+++ itext/itext.kernel/itext/kernel/pdf/annot/PdfTrapNetworkAnnotation.cs
@@ -49,7 +49,35 @@ using iText.Kernel.Pdf;
 using iText.Kernel.Pdf.Xobject;
 
 namespace iText.Kernel.Pdf.Annot {
+    /// <summary>
+    /// A
+    /// <see cref="PdfTrapNetworkAnnotation"/>
+    /// may be used to define the trapping characteristics for a page
+    /// of a PDF document. Trapping is the process of adding marks to a page along colour boundaries
+    /// to avoid unwanted visual artifacts resulting from misregistration of colorants when the page is printed.
+    /// See ISO-320001 14.11.6 "Trapping Support" and 14.11.6.2 "Trap Network Annotations" in particular.
+    /// </summary>
     public class PdfTrapNetworkAnnotation : PdfAnnotation {
+        /// <summary>
+        /// Creates a
+        /// <see cref="PdfTrapNetworkAnnotation"/>
+        /// instance. Note that there shall be at most one trap network annotation
+        /// per page, which shall be the last element in the page’s Annots array.
+        /// </summary>
+        /// <param name="rect">
+        /// the annotation rectangle, defining the location of the annotation on the page
+        /// in default user space units. See
+        /// <see cref="PdfAnnotation.SetRectangle(iText.Kernel.Pdf.PdfArray)"/>
+        /// .
+        /// </param>
+        /// <param name="appearanceStream">
+        /// the form XObject defining a trap network which body contains the graphics objects needed
+        /// to paint the traps making up the trap network. Process colour model shall be defined for the
+        /// appearance stream (see
+        /// <see cref="iText.Kernel.Pdf.Xobject.PdfFormXObject.SetProcessColorModel(iText.Kernel.Pdf.PdfName)"/>
+        /// .
+        /// See also ISO-320001 Table 367 "Additional entries specific to a trap network appearance stream".
+        /// </param>
         public PdfTrapNetworkAnnotation(Rectangle rect, PdfFormXObject appearanceStream)
             : base(rect) {
             if (appearanceStream.GetProcessColorModel() == null) {
@@ -60,43 +88,201 @@ namespace iText.Kernel.Pdf.Annot {
             SetFlags(PdfAnnotation.PRINT | PdfAnnotation.READ_ONLY);
         }
 
+        /// <summary>
+        /// Creates a
+        /// <see cref="PdfLineAnnotation"/>
+        /// instance from the given
+        /// <see cref="iText.Kernel.Pdf.PdfDictionary"/>
+        /// that represents annotation object. This method is useful for property reading in reading mode or
+        /// modifying in stamping mode.
+        /// </summary>
+        /// <param name="pdfObject">
+        /// a
+        /// <see cref="iText.Kernel.Pdf.PdfDictionary"/>
+        /// that represents existing annotation in the document.
+        /// </param>
         public PdfTrapNetworkAnnotation(PdfDictionary pdfObject)
             : base(pdfObject) {
         }
 
+        /// <summary><inheritDoc/></summary>
         public override PdfName GetSubtype() {
             return PdfName.TrapNet;
         }
 
+        /// <summary>The date and time when the trap network was most recently modified.</summary>
+        /// <remarks>
+        /// The date and time when the trap network was most recently modified.
+        /// <p>
+        /// This entry is required if /Version (
+        /// <see cref="GetVersion()"/>
+        /// ) and /AnnotStates (
+        /// <see cref="GetAnnotStates()"/>
+        /// )
+        /// entries are absent; shall be absent if /Version and /AnnotStates entries are present.
+        /// </remarks>
+        /// <param name="lastModified">
+        /// a
+        /// <see cref="iText.Kernel.Pdf.PdfDate"/>
+        /// wrapper with the specified date.
+        /// </param>
+        /// <returns>
+        /// this
+        /// <see cref="PdfTrapNetworkAnnotation"/>
+        /// instance.
+        /// </returns>
         public virtual iText.Kernel.Pdf.Annot.PdfTrapNetworkAnnotation SetLastModified(PdfDate lastModified) {
             return (iText.Kernel.Pdf.Annot.PdfTrapNetworkAnnotation)Put(PdfName.LastModified, lastModified.GetPdfObject
                 ());
         }
 
+        /// <summary>The date and time when the trap network was most recently modified.</summary>
+        /// <returns>
+        /// a
+        /// <see cref="iText.Kernel.Pdf.PdfString"/>
+        /// with date. The format should be a date string as described
+        /// in ISO-320001 7.9.4, “Dates”. See also
+        /// <see cref="iText.Kernel.Pdf.PdfDate.Decode(System.String)"/>
+        /// .
+        /// </returns>
         public virtual PdfString GetLastModified() {
             return GetPdfObject().GetAsString(PdfName.LastModified);
         }
 
+        /// <summary>
+        /// An unordered array of all objects present in the page description at the time the trap networks
+        /// were generated and that, if changed, could affect the appearance of the page.
+        /// </summary>
+        /// <remarks>
+        /// An unordered array of all objects present in the page description at the time the trap networks
+        /// were generated and that, if changed, could affect the appearance of the page.
+        /// <p>
+        /// This entry is required if /AnnotStates (
+        /// <see cref="GetAnnotStates()"/>
+        /// ) is present;
+        /// shall be absent if /LastModified (
+        /// <see cref="GetLastModified()"/>
+        /// ) is present.
+        /// </remarks>
+        /// <param name="version">
+        /// an unordered
+        /// <see cref="iText.Kernel.Pdf.PdfArray"/>
+        /// of all objects present in the page description at the time the trap networks
+        /// were generated. If present, the array shall include the following objects:
+        /// <ul>
+        /// <li>all page content streams;</li>
+        /// <li>all page resource objects (other than procedure sets);</li>
+        /// <li>all resource objects (other than procedure sets) of any form XObjects on the page;</li>
+        /// <li>all OPI dictionaries associated with XObjects on the page (see ISO-320001 14.11.7, “Open Prepress Interface (OPI)”)</li>
+        /// </ul>
+        /// </param>
+        /// <returns>
+        /// this
+        /// <see cref="PdfTrapNetworkAnnotation"/>
+        /// instance.
+        /// </returns>
         public virtual iText.Kernel.Pdf.Annot.PdfTrapNetworkAnnotation SetVersion(PdfArray version) {
             return (iText.Kernel.Pdf.Annot.PdfTrapNetworkAnnotation)Put(PdfName.Version, version);
         }
 
+        /// <summary>
+        /// An unordered array of all objects present in the page description at the time the trap networks were generated
+        /// and that, if changed, could affect the appearance of the page.
+        /// </summary>
+        /// <returns>
+        /// an unordered
+        /// <see cref="iText.Kernel.Pdf.PdfArray"/>
+        /// of all objects present in the page description at the time the trap networks
+        /// were generated.
+        /// </returns>
         public virtual PdfArray GetVersion() {
             return GetPdfObject().GetAsArray(PdfName.Version);
         }
 
+        /// <summary>
+        /// An array of name objects representing the appearance states (value of the /AS entry
+        /// <see cref="PdfAnnotation.GetAppearanceState()"/>
+        /// )
+        /// for annotations associated with the page. The appearance states shall be listed in the same order as the annotations
+        /// in the page’s /Annots array. For an annotation with no /AS entry, the corresponding array element
+        /// should be
+        /// <see cref="iText.Kernel.Pdf.PdfNull"/>
+        /// .
+        /// No appearance state shall be included for the trap network annotation itself.
+        /// <p>
+        /// Required if /Version (
+        /// <see cref="GetVersion()"/>
+        /// ) is present; shall be absent if /LastModified
+        /// <see cref="GetLastModified()"/>
+        /// is present.
+        /// </summary>
+        /// <param name="annotStates">
+        /// a
+        /// <see cref="iText.Kernel.Pdf.PdfArray"/>
+        /// of name objects representing the appearance states for annotations associated with the page.
+        /// </param>
+        /// <returns>
+        /// this
+        /// <see cref="PdfTrapNetworkAnnotation"/>
+        /// instance.
+        /// </returns>
         public virtual iText.Kernel.Pdf.Annot.PdfTrapNetworkAnnotation SetAnnotStates(PdfArray annotStates) {
             return (iText.Kernel.Pdf.Annot.PdfTrapNetworkAnnotation)Put(PdfName.AnnotStates, annotStates);
         }
 
+        /// <summary>An array of name objects representing the appearance states for annotations associated with the page.
+        ///     </summary>
+        /// <remarks>
+        /// An array of name objects representing the appearance states for annotations associated with the page.
+        /// See also
+        /// <see cref="SetAnnotStates(iText.Kernel.Pdf.PdfArray)"/>
+        /// .
+        /// </remarks>
+        /// <returns>
+        /// a
+        /// <see cref="iText.Kernel.Pdf.PdfArray"/>
+        /// of name objects representing the appearance states for annotations associated with the page,
+        /// </returns>
         public virtual PdfArray GetAnnotStates() {
             return GetPdfObject().GetAsArray(PdfName.AnnotStates);
         }
 
+        /// <summary>
+        /// An array of font dictionaries representing fonts that were fauxed (replaced by substitute fonts) during the
+        /// generation of trap networks for the page.
+        /// </summary>
+        /// <param name="fauxedFonts">
+        /// a
+        /// <see cref="iText.Kernel.Pdf.PdfArray"/>
+        /// of
+        /// <see cref="iText.Kernel.Pdf.PdfDictionary"/>
+        /// each of which represent font in the document.
+        /// </param>
+        /// <returns>
+        /// this
+        /// <see cref="PdfTrapNetworkAnnotation"/>
+        /// instance.
+        /// </returns>
         public virtual iText.Kernel.Pdf.Annot.PdfTrapNetworkAnnotation SetFauxedFonts(PdfArray fauxedFonts) {
             return (iText.Kernel.Pdf.Annot.PdfTrapNetworkAnnotation)Put(PdfName.FontFauxing, fauxedFonts);
         }
 
+        /// <summary>
+        /// A list of font dictionaries representing fonts that were fauxed (replaced by substitute fonts) during the
+        /// generation of trap networks for the page.
+        /// </summary>
+        /// <param name="fauxedFonts">
+        /// a
+        /// <see cref="System.Collections.IList{E}"/>
+        /// of
+        /// <see cref="iText.Kernel.Font.PdfFont"/>
+        /// objects.
+        /// </param>
+        /// <returns>
+        /// this
+        /// <see cref="PdfTrapNetworkAnnotation"/>
+        /// instance.
+        /// </returns>
         public virtual iText.Kernel.Pdf.Annot.PdfTrapNetworkAnnotation SetFauxedFonts(IList<PdfFont> fauxedFonts) {
             PdfArray arr = new PdfArray();
             foreach (PdfFont f in fauxedFonts) {
@@ -105,6 +291,17 @@ namespace iText.Kernel.Pdf.Annot {
             return SetFauxedFonts(arr);
         }
 
+        /// <summary>
+        /// An array of font dictionaries representing fonts that were fauxed (replaced by substitute fonts) during the
+        /// generation of trap networks for the page.
+        /// </summary>
+        /// <returns>
+        /// a
+        /// <see cref="iText.Kernel.Pdf.PdfArray"/>
+        /// of
+        /// <see cref="iText.Kernel.Pdf.PdfDictionary"/>
+        /// each of which represent font in the document.
+        /// </returns>
         public virtual PdfArray GetFauxedFonts() {
             return GetPdfObject().GetAsArray(PdfName.FontFauxing);
         }
