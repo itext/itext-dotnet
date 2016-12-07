9b1b08fe694b7e452d07789b08524b3d84140116
Yulian Gaponenko <yulian.gaponenko@duallab.com>
2016-12-07 12:38:05 +0300

    Add private setters for the Version property of custom assembly attributes
    DEVSIX-949

--- itext/itext.kernel/Properties/KeyVersionAttribute.cs
+++ itext/itext.kernel/Properties/KeyVersionAttribute.cs
@@ -3,7 +3,7 @@
 namespace Versions.Attributes {
     [AttributeUsage(AttributeTargets.Assembly)]
     internal class KeyVersionAttribute : Attribute {
-        internal string KeyVersion { get; }
+        internal string KeyVersion { get; private set; }
 
         internal KeyVersionAttribute(string keyVersion) {
             this.KeyVersion = keyVersion;
--- itext/itext.layout/Properties/TypographyVersionAttribute.cs
+++ itext/itext.layout/Properties/TypographyVersionAttribute.cs
@@ -3,7 +3,7 @@
 namespace Versions.Attributes {
     [AttributeUsage(AttributeTargets.Assembly)]
     internal class TypographyVersionAttribute : Attribute {
-        internal string TypographyVersion { get; }
+        internal string TypographyVersion { get; private set; }
 
         internal TypographyVersionAttribute(string typographyVersion) {
             this.TypographyVersion = typographyVersion;
