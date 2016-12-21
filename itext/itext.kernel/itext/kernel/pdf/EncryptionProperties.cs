2f0b9975a008fdf6f383e09056ea328209b35e90
Samuel Huylebroeck <samuel.huylebroeck@itextpdf.com>
2016-12-21 16:25:25 +0300

    Fix behavior of encryption properties not following declared behavior
    Fill owner password with a random string if null gets passed.
    
    Autoported commit.
    Original commit hash: [2aa97dd]

--- itext/itext.kernel/itext/kernel/pdf/EncryptionProperties.cs
+++ itext/itext.kernel/itext/kernel/pdf/EncryptionProperties.cs
@@ -41,6 +41,7 @@ source product.
 For more information, please contact iText Software Corp. at this
 address: sales@itextpdf.com
 */
+using System;
 using Org.BouncyCastle.X509;
 
 namespace iText.Kernel.Pdf {
@@ -83,7 +84,13 @@ namespace iText.Kernel.Pdf {
             , int encryptionAlgorithm) {
             ClearEncryption();
             this.userPassword = userPassword;
-            this.ownerPassword = ownerPassword;
+            if (ownerPassword != null) {
+                this.ownerPassword = ownerPassword;
+            }
+            else {
+                int r = (int)(int.MaxValue * iText.IO.Util.JavaUtil.Random());
+                this.ownerPassword = iText.IO.Util.JavaUtil.IntegerToHexString(r).GetBytes();
+            }
             this.standardEncryptPermissions = permissions;
             this.encryptionAlgorithm = encryptionAlgorithm;
             return this;
