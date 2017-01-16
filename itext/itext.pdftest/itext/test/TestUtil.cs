c35d7f309353d9d0a7b36cd750d617ba013f91a1
Alexey Subach <alexey.subach@duallab.com>
2017-01-16 15:28:44 +0300

    Add TestUtil.GetParentProjectDirectory method for getting base project path by the location of binary output folder
    DEVSIX-945

---
+++ itext/itext.pdftest/itext/test/TestUtil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace iText.Test {
+    public static class TestUtil
+    {
+        public static String GetParentProjectDirectory(String testDirectory) {
+            DirectoryInfo projectDirectoryInfo = new DirectoryInfo(testDirectory);
+            while (!projectDirectoryInfo.Name.Equals("bin", StringComparison.OrdinalIgnoreCase)) {
+                projectDirectoryInfo = projectDirectoryInfo.Parent;
+            }
+            return projectDirectoryInfo.Parent.FullName;
+        }
+    }
+}
