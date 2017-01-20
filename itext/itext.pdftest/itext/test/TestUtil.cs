8f6f0b7f001ca51598bd9cdb49509e2ba9721617
Yulian Gaponenko <yulian.gaponenko@duallab.com>
2017-01-20 17:01:57 +0300

    Minor reformatting

--- itext/itext.pdftest/itext/test/TestUtil.cs
+++ itext/itext.pdftest/itext/test/TestUtil.cs
@@ -2,8 +2,7 @@
 using System.IO;
 
 namespace iText.Test {
-    public static class TestUtil
-    {
+    public static class TestUtil {
         public static String GetParentProjectDirectory(String testDirectory) {
             DirectoryInfo projectDirectoryInfo = new DirectoryInfo(testDirectory);
             while (!projectDirectoryInfo.Name.Equals("bin", StringComparison.OrdinalIgnoreCase)) {
