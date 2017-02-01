affab866a5d9f2d5be29a3c1243e6332109d653d
Vladimir Osipchuk <vladimir.osipchuk@duallab.com>
2017-02-01 19:33:05 +0300

    Set default minMaxWidth in MinMaxWidthLayoutResult
    Autoported commit.
    Original commit hash: [411d665]

--- itext/itext.layout/itext/layout/layout/MinMaxWidthLayoutResult.cs
+++ itext/itext.layout/itext/layout/layout/MinMaxWidthLayoutResult.cs
@@ -56,11 +56,13 @@ namespace iText.Layout.Layout {
         public MinMaxWidthLayoutResult(int status, LayoutArea occupiedArea, IRenderer splitRenderer, IRenderer overflowRenderer
             )
             : base(status, occupiedArea, splitRenderer, overflowRenderer) {
+            minMaxWidth = new MinMaxWidth(0, 0);
         }
 
         public MinMaxWidthLayoutResult(int status, LayoutArea occupiedArea, IRenderer splitRenderer, IRenderer overflowRenderer
             , IRenderer cause)
             : base(status, occupiedArea, splitRenderer, overflowRenderer, cause) {
+            minMaxWidth = new MinMaxWidth(0, 0);
         }
 
         public virtual MinMaxWidth GetNotNullMinMaxWidth(float availableWidth) {
