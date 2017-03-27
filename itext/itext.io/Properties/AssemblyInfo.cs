1fb3d8ac23e88b2000ead8dbb75a049ddd82b145
Pavel Alay <pavel.alay@duallab.com>
2017-03-27 14:08:40 +0300

    Update AssemblyVersion: 7.0.2.3 ->7.0.3.0. Add AssemblyInformationalVersion: 7.0.3-SNAPSHOT.

--- itext/itext.io/Properties/AssemblyInfo.cs
+++ itext/itext.io/Properties/AssemblyInfo.cs
@@ -21,5 +21,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("39631ecb-1d39-4eb2-b775-37bd34cbf5a4")]
 
-[assembly: AssemblyVersion("7.0.2.3")]
-[assembly: AssemblyFileVersion("7.0.2.3")]
+[assembly: AssemblyVersion("7.0.3.0")]
+[assembly: AssemblyFileVersion("7.0.3.0")]
+[assembly: AssemblyInformationalVersion("7.0.3-SNAPSHOT")]
