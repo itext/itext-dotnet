ab2c2a6fb8e27012668ec385b4635ec5ab6d770a
Valera Goman <valera.goman@duallab.com>
2017-07-17 17:22:34 +0300

    Add MathUtil with toRadians method
    DEVSIX-1310

---
+++ itext/itext.io/itext/io/util/MathUtil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace iText.IO.Util
+{
+    public static class MathUtil
+    {
+        public static double ToRadians(double angdeg)
+        {
+            return angdeg / 180.0 * Math.PI;
+        }
+    }
+}
