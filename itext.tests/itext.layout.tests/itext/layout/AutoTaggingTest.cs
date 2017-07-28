a1768875e8c9fde3acf19531e12329d1848bf569
Yulian Gaponenko <yulian.gaponenko@duallab.com>
2017-07-28 10:48:05 +0300

    Add a test showing issues with Table element tagging when it's role set to null
    DEVSIX-1463
    
    Autoported commit.
    Original commit hash: [e6dfc72c13]

--- itext.tests/itext.layout.tests/itext/layout/AutoTaggingTest.cs
+++ itext.tests/itext.layout.tests/itext/layout/AutoTaggingTest.cs
@@ -801,6 +801,32 @@ namespace iText.Layout {
         }
 
         /// <exception cref="System.IO.IOException"/>
+        /// <exception cref="Javax.Xml.Parsers.ParserConfigurationException"/>
+        /// <exception cref="Org.Xml.Sax.SAXException"/>
+        /// <exception cref="System.Exception"/>
+        [NUnit.Framework.Test]
+        [NUnit.Framework.Ignore("DEVSIX-1463")]
+        public virtual void TableWithCaption01() {
+            PdfWriter writer = new PdfWriter(destinationFolder + "tableWithCaption01.pdf");
+            PdfDocument pdf = new PdfDocument(writer);
+            Document document = new Document(pdf);
+            pdf.SetTagged();
+            Paragraph p;
+            p = new Paragraph("We try to create a Table with a Caption by creating a Div with two children: " + "a Div that is a caption and a Table. "
+                 + "To tag this correctly, I set the outer Div role to Table, the inner Div to Caption, and the " + "Table to null."
+                );
+            document.Add(p);
+            p = new Paragraph("This table is tagged correctly.");
+            document.Add(p);
+            document.Add(CreateTable(false));
+            p = new Paragraph("This table has a caption and is tagged incorrectly. ");
+            document.Add(p);
+            document.Add(CreateTable(true));
+            document.Close();
+            CompareResult("tableWithCaption01.pdf", "cmp_tableWithCaption01.pdf");
+        }
+
+        /// <exception cref="System.IO.IOException"/>
         private Paragraph CreateParagraph1() {
             PdfFont font = PdfFontFactory.CreateFont(FontConstants.HELVETICA_BOLD);
             Paragraph p = new Paragraph().Add("text chunk. ").Add("explicitly added separate text chunk");
@@ -820,6 +846,34 @@ namespace iText.Layout {
             String longText = longTextBuilder.ToString();
             p = new Paragraph(longText);
             return p;
+        }
+
+        private IBlockElement CreateTable(bool useCaption) {
+            Table table = new Table(new float[3]).SetMarginTop(10).SetMarginBottom(10);
+            for (int r = 0; r < 2; r++) {
+                for (int c = 0; c < 3; c++) {
+                    String content = r + "," + c;
+                    Cell cell = new Cell();
+                    cell.Add(content);
+                    table.AddCell(cell);
+                }
+            }
+            if (useCaption) {
+                Div div = new Div();
+                div.SetRole(PdfName.Table);
+                Paragraph p = new Paragraph("Caption");
+                p.SetRole(null);
+                p.SetTextAlignment(TextAlignment.CENTER).SetBold();
+                Div caption = new Div().Add(p);
+                caption.SetRole(PdfName.Caption);
+                div.Add(caption);
+                table.SetRole(null);
+                div.Add(table);
+                return div;
+            }
+            else {
+                return table;
+            }
         }
 
         /// <exception cref="System.IO.IOException"/>
