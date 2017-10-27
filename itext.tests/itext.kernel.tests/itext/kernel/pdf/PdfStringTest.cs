5920cda93d6e3b5ea89caa3a0f395894bbb69af2
Valera Goman <valera.goman@duallab.com>
2017-10-27 21:28:46 +0300

    Add tests for utf-8
    DEVSIX-1089
    
    Autoported commit.
    Original commit hash: [963471c19]

--- itext.tests/itext.kernel.tests/itext/kernel/pdf/PdfStringTest.cs
+++ itext.tests/itext.kernel.tests/itext/kernel/pdf/PdfStringTest.cs
@@ -41,8 +41,18 @@ For more information, please contact iText Software Corp. at this
 address: sales@itextpdf.com
 */
 using System;
+using System.Collections.Generic;
+using iText.IO.Font;
+using iText.IO.Image;
+using iText.Kernel.Colors;
+using iText.Kernel.Font;
+using iText.Kernel.Pdf.Canvas;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
+using iText.Kernel.Pdf.Tagutils;
 using iText.Kernel.Utils;
 using iText.Test;
+using iText.Test.Attributes;
 
 namespace iText.Kernel.Pdf {
     public class PdfStringTest : ExtendedITextTest {
@@ -93,6 +103,191 @@ namespace iText.Kernel.Pdf {
             String unicode = "Привет!";
             PdfString @string = new PdfString(unicode);
             NUnit.Framework.Assert.AreNotEqual(unicode, @string.ToUnicodeString());
+        }
+
+        /// <exception cref="System.IO.IOException"/>
+        /// <exception cref="System.Exception"/>
+        [NUnit.Framework.Test]
+        public virtual void ReadUtf8ActualText() {
+            String filename = sourceFolder + "utf-8-actual-text.pdf";
+            PdfDocument pdfDoc = new PdfDocument(new PdfReader(filename));
+            String text = PdfTextExtractor.GetTextFromPage(pdfDoc.GetPage(1), new LocationTextExtractionStrategy().SetUseActualText
+                (true));
+            pdfDoc.Close();
+            //  शांति देवनागरी
+            NUnit.Framework.Assert.AreEqual("\u0936\u093e\u0902\u0924\u093f \u0926\u0947\u0935\u0928\u093E\u0917\u0930\u0940"
+                , text);
+        }
+
+        /// <exception cref="System.IO.IOException"/>
+        /// <exception cref="System.Exception"/>
+        [NUnit.Framework.Test]
+        [LogMessage(iText.IO.LogMessageConstant.EXISTING_TAG_STRUCTURE_ROOT_IS_NOT_STANDARD)]
+        public virtual void ReadUtf8AltText() {
+            String filename = sourceFolder + "utf-8-alt-text.pdf";
+            PdfDocument pdfDoc = new PdfDocument(new PdfReader(filename), new PdfWriter(destinationFolder + "whatever"
+                ));
+            TagTreePointer tagTreePointer = new TagTreePointer(pdfDoc);
+            String alternateDescription = tagTreePointer.MoveToKid(0).MoveToKid(0).MoveToKid(0).GetProperties().GetAlternateDescription
+                ();
+            pdfDoc.Close();
+            //  2001: A Space Odyssey (Космическая одиссея)
+            NUnit.Framework.Assert.AreEqual("2001: A Space Odyssey (\u041A\u043E\u0441\u043C\u0438\u0447\u0435\u0441\u043A\u0430\u044F "
+                 + "\u043E\u0434\u0438\u0441\u0441\u0435\u044F)", alternateDescription);
+        }
+
+        /// <exception cref="System.IO.IOException"/>
+        /// <exception cref="System.Exception"/>
+        [NUnit.Framework.Test]
+        public virtual void ReadUtf8Bookmarks() {
+            String filename = sourceFolder + "utf-8-bookmarks.pdf";
+            PdfDocument pdfDoc = new PdfDocument(new PdfReader(filename));
+            PdfOutline outline = pdfDoc.GetOutlines(true);
+            IList<String> children = new List<String>(6);
+            foreach (PdfOutline child in outline.GetAllChildren()) {
+                children.Add(child.GetTitle());
+                foreach (PdfOutline childOfChild in child.GetAllChildren()) {
+                    children.Add(childOfChild.GetTitle());
+                }
+            }
+            pdfDoc.Close();
+            IList<String> expected = new List<String>(6);
+            //  福昕
+            expected.Add("\u798F\u6615 bookmark 1");
+            expected.Add("\u798F\u6615  bookmark 1-1");
+            expected.Add("\u798F\u6615  bookmark 1-2");
+            //  中国
+            expected.Add("\u4E2D\u56FD bookmark 2");
+            expected.Add("\u4E2D\u56FD  bookmark 2-1");
+            expected.Add("\u4E2D\u56FD  bookmark 2-2");
+            for (int i = 0; i < 6; i++) {
+                NUnit.Framework.Assert.AreEqual(expected[i], children[i]);
+            }
+        }
+
+        /// <exception cref="System.IO.IOException"/>
+        /// <exception cref="System.Exception"/>
+        [NUnit.Framework.Test]
+        public virtual void ReadUtf8PageLabelPrefix() {
+            String filename = sourceFolder + "utf-8-page-label-prefix.pdf";
+            PdfDocument pdfDoc = new PdfDocument(new PdfReader(filename));
+            String[] labels = pdfDoc.GetPageLabels();
+            String[] expected = new String[] { "A", "B", "1", "2", "3", "4", "Movies-5", "Movies-6", "Movies-7", "Movies-8"
+                , "Movies-9", "Movies-10", "Movies-11", "Movies-12" };
+            pdfDoc.Close();
+            for (int i = 0; i < labels.Length; i++) {
+                NUnit.Framework.Assert.AreEqual(expected[i], labels[i]);
+            }
+        }
+
+        /// <exception cref="System.IO.IOException"/>
+        /// <exception cref="System.Exception"/>
+        [NUnit.Framework.Test]
+        public virtual void WriteUtf8AltText() {
+            String RESOURCE = sourceFolder + "Space Odyssey.jpg";
+            PdfDocument pdfDoc = new PdfDocument(new PdfWriter(destinationFolder + "writeUtf8AltText.pdf"));
+            pdfDoc.SetTagged();
+            PdfPage page = pdfDoc.AddNewPage();
+            PdfCanvas canvas = new PdfCanvas(page);
+            TagTreePointer tagPointer = new TagTreePointer(pdfDoc);
+            tagPointer.SetPageForTagging(page);
+            tagPointer.AddTag(PdfName.Div);
+            tagPointer.AddTag(PdfName.Span);
+            //  2001: A Space Odyssey (Космическая одиссея)
+            tagPointer.GetContext().GetPointerStructElem(tagPointer).SetAlt(new PdfString("2001: A Space Odyssey (\u041A\u043E\u0441\u043C\u0438\u0447\u0435\u0441\u043A\u0430\u044F "
+                 + "\u043E\u0434\u0438\u0441\u0441\u0435\u044F)", PdfEncodings.UTF8));
+            ImageData img = ImageDataFactory.Create(RESOURCE);
+            canvas.OpenTag(tagPointer.GetTagReference());
+            canvas.AddImage(img, 36, 700, 100, false, false);
+            canvas.CloseTag();
+            canvas.EndText();
+            pdfDoc.Close();
+            NUnit.Framework.Assert.IsNull(new CompareTool().CompareByContent(destinationFolder + "writeUtf8AltText.pdf"
+                , sourceFolder + "cmp_writeUtf8AltText.pdf", destinationFolder, "diffAltText_"));
+        }
+
+        /// <exception cref="System.IO.IOException"/>
+        /// <exception cref="System.Exception"/>
+        [NUnit.Framework.Test]
+        public virtual void WriteUtf8Bookmarks() {
+            PdfDocument pdfDoc = new PdfDocument(new PdfWriter(destinationFolder + "writeUtf8Bookmarks.pdf"));
+            PdfPage page = pdfDoc.AddNewPage();
+            PdfCanvas canvas = new PdfCanvas(page);
+            canvas.SetFillColor(ColorConstants.MAGENTA);
+            canvas.BeginText();
+            canvas.SetFontAndSize(PdfFontFactory.CreateFont(FontConstants.TIMES_ROMAN), 30);
+            canvas.SetTextMatrix(25, 500);
+            canvas.ShowText("This file has bookmarks encoded with utf-8");
+            canvas.EndText();
+            PdfOutline root = pdfDoc.GetOutlines(false);
+            PdfOutline first = root.AddOutline("");
+            //  福昕
+            first.GetContent().Put(PdfName.Title, new PdfString("\u798F\u6615 bookmark 1", PdfEncodings.UTF8));
+            first.AddOutline("").GetContent().Put(PdfName.Title, new PdfString("\u798F\u6615  bookmark 1-1", PdfEncodings
+                .UTF8));
+            first.AddOutline("").GetContent().Put(PdfName.Title, new PdfString("\u798F\u6615  bookmark 1-2", PdfEncodings
+                .UTF8));
+            PdfOutline second = root.AddOutline("");
+            //  中国
+            second.GetContent().Put(PdfName.Title, new PdfString("\u4E2D\u56FD bookmark 2", PdfEncodings.UTF8));
+            second.AddOutline("").GetContent().Put(PdfName.Title, new PdfString("\u4E2D\u56FD  bookmark 2-1", PdfEncodings
+                .UTF8));
+            second.AddOutline("").GetContent().Put(PdfName.Title, new PdfString("\u4E2D\u56FD  bookmark 2-2", PdfEncodings
+                .UTF8));
+            pdfDoc.Close();
+            NUnit.Framework.Assert.IsNull(new CompareTool().CompareByContent(destinationFolder + "writeUtf8Bookmarks.pdf"
+                , sourceFolder + "cmp_writeUtf8Bookmarks.pdf", destinationFolder, "diffBookmarks_"));
+        }
+
+        /// <exception cref="System.IO.IOException"/>
+        /// <exception cref="System.Exception"/>
+        [NUnit.Framework.Test]
+        public virtual void WriteUtf8PageLabelPrefix() {
+            PdfDocument pdfDoc = new PdfDocument(new PdfWriter(destinationFolder + "writeUtf8PageLabelPrefix.pdf"));
+            PdfPage page = pdfDoc.AddNewPage();
+            PdfDictionary pageLabel = new PdfDictionary();
+            pageLabel.Put(PdfName.S, PdfName.D);
+            pageLabel.Put(PdfName.P, new PdfString("PREFIX-", PdfEncodings.UTF8));
+            pageLabel.Put(PdfName.St, new PdfNumber(1));
+            pdfDoc.GetCatalog().GetPageLabelsTree(true).AddEntry(pdfDoc.GetPageNumber(page) - 1, pageLabel);
+            PdfCanvas canvas = new PdfCanvas(page);
+            canvas.SetFillColor(ColorConstants.MAGENTA);
+            canvas.BeginText();
+            canvas.SetFontAndSize(PdfFontFactory.CreateFont(FontConstants.TIMES_ROMAN), 30);
+            canvas.SetTextMatrix(25, 500);
+            String text = "This page has pageLabel prefix " + "PREFIX-";
+            canvas.ShowText(text);
+            canvas.EndText();
+            pdfDoc.Close();
+            NUnit.Framework.Assert.IsNull(new CompareTool().CompareByContent(destinationFolder + "writeUtf8PageLabelPrefix.pdf"
+                , sourceFolder + "cmp_writeUtf8PageLabelPrefix.pdf", destinationFolder, "diffPageLabelPrefix_"));
+        }
+
+        /// <exception cref="System.IO.IOException"/>
+        /// <exception cref="System.Exception"/>
+        [NUnit.Framework.Test]
+        public virtual void WriteUtf8ActualText() {
+            PdfDocument pdfDoc = new PdfDocument(new PdfWriter(destinationFolder + "writeUtf8ActualText.pdf"));
+            pdfDoc.SetTagged();
+            PdfPage page = pdfDoc.AddNewPage();
+            PdfCanvas canvas = new PdfCanvas(page);
+            TagTreePointer tagPointer = new TagTreePointer(pdfDoc);
+            tagPointer.SetPageForTagging(page);
+            tagPointer.AddTag(PdfName.Div);
+            tagPointer.AddTag(PdfName.Span);
+            tagPointer.GetContext().GetPointerStructElem(tagPointer).SetActualText(new PdfString("actual", PdfEncodings
+                .UTF8));
+            canvas.BeginText();
+            canvas.MoveText(36, 788);
+            canvas.SetFontAndSize(PdfFontFactory.CreateFont(FontConstants.TIMES_ROMAN), 12);
+            canvas.OpenTag(tagPointer.GetTagReference());
+            canvas.ShowText("These piece of text has an actual text property. Can be viewed via properties of span in the tag tree."
+                );
+            canvas.CloseTag();
+            canvas.EndText();
+            pdfDoc.Close();
+            NUnit.Framework.Assert.IsNull(new CompareTool().CompareByContent(destinationFolder + "writeUtf8ActualText.pdf"
+                , sourceFolder + "cmp_writeUtf8ActualText.pdf", destinationFolder, "diffActualText_"));
         }
     }
 }
