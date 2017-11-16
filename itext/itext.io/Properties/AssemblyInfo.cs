617d6fc28f9fa7ea29f55177221f390a43607f16
Pavel Alay <pavel.alay@duallab.com>
2017-11-16 23:09:16 +0300

    Mark kernel as friendly module to io.
    DEVSIX-1468

--- itext/itext.io/Properties/AssemblyInfo.cs
+++ itext/itext.io/Properties/AssemblyInfo.cs
@@ -17,6 +17,13 @@ using System.Runtime.InteropServices;
                               "75bcdbcecb7caf1f0f4b6e7d013906ba60b66eb1c8298e4efb052caf6cece4bf1816" +
                               "902cc")]
 
+[assembly: InternalsVisibleTo("itext.kernel,PublicKey=0024000004800000940000000602000000240000525" +
+                              "3413100040000010001008b21ed5b3fc1c11996390981fe22bbe71a39a9e11d3c2ce" +
+                              "fddd6ee92920fa871f9666ae0fa941af0280d0653df048ae2d93f8c5e2d820dba3c8" +
+                              "df9ed468c8be40a6fffeb32aa481a254f0fb9f37aa7c3ec1c0acd2c009746bbdafcb" +
+                              "75bcdbcecb7caf1f0f4b6e7d013906ba60b66eb1c8298e4efb052caf6cece4bf1816" +
+                              "902cc")]
+
 [assembly: ComVisible(false)]
 
 [assembly: Guid("39631ecb-1d39-4eb2-b775-37bd34cbf5a4")]
