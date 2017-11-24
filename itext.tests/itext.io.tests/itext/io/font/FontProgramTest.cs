694f1a398ef572cdd37ef742dc1f6dbcc8f30021
Ilya Idamkin <ilya.idamkin@duallab.com>
2017-11-24 20:16:11 +0300

    Refactor exception message test
    Autoported commit.
    Original commit hash: [1629a68f9]

--- itext.tests/itext.io.tests/itext/io/font/FontProgramTest.cs
+++ itext.tests/itext.io.tests/itext/io/font/FontProgramTest.cs
@@ -54,7 +54,7 @@ namespace iText.IO.Font {
             NUnit.Framework.Assert.That(() =>  {
                 FontProgramFactory.CreateFont(notExistingFont);
             }
-            , NUnit.Framework.Throws.TypeOf<System.IO.IOException>().With.Message.EqualTo(MessageFormatUtil.Format("{0} not found as file or resource", notExistingFont)));
+            , NUnit.Framework.Throws.TypeOf<System.IO.IOException>().With.Message.EqualTo(MessageFormatUtil.Format(iText.IO.IOException._1NotFoundAsFileOrResource, notExistingFont)));
 ;
         }
 
