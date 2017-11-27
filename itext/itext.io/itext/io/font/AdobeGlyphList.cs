b6a437645d6b8a1626e3b69b499ca4f3122d0c60
Pavel Alay <pavel.alay@duallab.com>
2017-11-27 17:00:21 +0300

    Update exception message in AdobeGlyphList.
    DEVSIX-1013
    
    Autoported commit.
    Original commit hash: [0d6e4fe6d]

--- itext/itext.io/itext/io/font/AdobeGlyphList.cs
+++ itext/itext.io/itext/io/font/AdobeGlyphList.cs
@@ -58,8 +58,7 @@ namespace iText.IO.Font {
             try {
                 resource = ResourceUtil.GetResourceStream(FontResources.ADOBE_GLYPH_LIST);
                 if (resource == null) {
-                    String msg = "AdobeGlyphList.txt not found as resource. (It must exist as resource in the package com.itextpdf.text.pdf.fonts)";
-                    throw new Exception(msg);
+                    throw new Exception(FontResources.ADOBE_GLYPH_LIST + " not found as resource.");
                 }
                 byte[] buf = new byte[1024];
                 MemoryStream stream = new MemoryStream();
