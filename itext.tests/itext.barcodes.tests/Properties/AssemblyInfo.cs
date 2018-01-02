994b415a518ccd41aa35f8dd003ab76709751919
Leeroy Jenkins <leeroy@itextpdf.com>
2018-01-02 13:57:20 +0100

    Add missing copyright headers
    Autoported commit.
    Original commit hash: [51c789761]

--- itext.tests/itext.barcodes.tests/Properties/AssemblyInfo.cs
+++ itext.tests/itext.barcodes.tests/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("iText Group NV")]
 [assembly: AssemblyProduct ("iText")]
-[assembly: AssemblyCopyright ("Copyright (c) 1998-2017 iText Group NV")]
+[assembly: AssemblyCopyright ("Copyright (c) 1998-2018 iText Group NV")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
