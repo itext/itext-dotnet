1542f8c1851d388b0bd55f9bffcf5e109896c149
Amedee Van Gasse <amedee.vangasse@itextpdf.com>
2018-03-09 11:08:07 +0300

    Add javadoc
    Autoported commit.
    Original commit hash: [25f6324]

--- itext/itext.svg/itext/svg/Foo.cs
+++ itext/itext.svg/itext/svg/Foo.cs
@@ -1,7 +1,9 @@
 using System;
 
 namespace iText.Svg {
+    /// <author>Amedee Van Gasse</author>
     public class Foo {
+        /// <summary>Returns a common phrase for code examples</summary>
         public override String ToString() {
             return "Hello, world";
         }
