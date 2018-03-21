1ee008d9640495b5f4624e3051050bf5f3ec7fde
Michael Demey <michael.demey@itextpdf.com>
2018-03-21 18:54:02 +0300

    Revert "Introducing instantiation in TransformUtils to test sonarqube"
    This reverts commit 06fb46a47ee06d5c783c62a23bf73d67a9163caf.
    
    Autoported commit.
    Original commit hash: [4ac026e]

--- itext/itext.svg/itext/svg/utils/TransformUtils.cs
+++ itext/itext.svg/itext/svg/utils/TransformUtils.cs
@@ -134,9 +134,8 @@ namespace iText.Svg.Utils {
             if (String.IsNullOrEmpty(transform)) {
                 throw new SvgProcessingException(SvgLogMessageConstant.TRANSFORM_EMPTY);
             }
-            iText.Svg.Utils.TransformUtils utils = new iText.Svg.Utils.TransformUtils();
             AffineTransform matrix = new AffineTransform();
-            IList<String> listWithTransformations = iText.Svg.Utils.TransformUtils.SplitString(transform);
+            IList<String> listWithTransformations = SplitString(transform);
             foreach (String transformation in listWithTransformations) {
                 AffineTransform newMatrix = TransformationStringToMatrix(transformation);
                 if (newMatrix != null) {
