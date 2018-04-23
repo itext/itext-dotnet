8a5fdb91aaa7651e4644ae5ab0e2eb7e717f4b53
Pavel Alay <pavel.alay@duallab.com>
2018-04-23 07:55:13 +0300

    [RELEASE] 1.0.0-SNAPSHOT -> 7.1.2

--- itext/itext.styledxmlparser/Properties/AssemblyInfo.cs
+++ itext/itext.styledxmlparser/Properties/AssemblyInfo.cs
@@ -37,7 +37,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-SNAPSHOT")]
+// [assembly: AssemblyVersion("7.1.2.0")]
+[assembly: AssemblyVersion("7.1.2.0")]
+[assembly: AssemblyFileVersion("7.1.2.0")]
+[assembly: AssemblyInformationalVersion("7.1.2")]
