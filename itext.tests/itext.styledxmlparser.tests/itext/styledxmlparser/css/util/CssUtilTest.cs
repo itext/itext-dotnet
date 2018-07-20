9e623956dc9221d950e9d6b58679ed351cf1bbe7
Vladimir Osipchuk <vladimir.osipchuk@duallab.com>
2018-07-20 12:12:13 +0300

    Merge CssUtilsTest and CssUtilTest.
    DEVSIX-1998
    DEVSIX-2053
    
    Autoported commit.
    Original commit hash: [1e50abb]

--- itext.tests/itext.styledxmlparser.tests/itext/styledxmlparser/css/util/CssUtilTest.cs
+++ itext.tests/itext.styledxmlparser.tests/itext/styledxmlparser/css/util/CssUtilTest.cs
@@ -40,11 +40,76 @@ source product.
 For more information, please contact iText Software Corp. at this
 address: sales@itextpdf.com
 */
+using System;
+using iText.IO.Util;
 using iText.StyledXmlParser.Css;
+using iText.StyledXmlParser.Exceptions;
 using iText.Test;
+using iText.Test.Attributes;
 
 namespace iText.StyledXmlParser.Css.Util {
     public class CssUtilTest : ExtendedITextTest {
+        [NUnit.Framework.Test]
+        public virtual void ParseAbsoluteLengthFromNAN() {
+            NUnit.Framework.Assert.That(() =>  {
+                String value = "Definitely not a number";
+                CssUtils.ParseAbsoluteLength(value);
+            }
+            , NUnit.Framework.Throws.TypeOf<StyledXMLParserException>().With.Message.EqualTo(MessageFormatUtil.Format(iText.StyledXmlParser.LogMessageConstant.NAN, "Definitely not a number")));
+;
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void ParseAbsoluteLengthFromNull() {
+            NUnit.Framework.Assert.That(() =>  {
+                String value = null;
+                CssUtils.ParseAbsoluteLength(value);
+            }
+            , NUnit.Framework.Throws.TypeOf<StyledXMLParserException>().With.Message.EqualTo(MessageFormatUtil.Format(iText.StyledXmlParser.LogMessageConstant.NAN, "null")));
+;
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void ParseAbsoluteLengthFrom10px() {
+            String value = "10px";
+            float actual = CssUtils.ParseAbsoluteLength(value, CommonCssConstants.PX);
+            float expected = 7.5f;
+            NUnit.Framework.Assert.AreEqual(expected, actual, 0);
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void ParseAbsoluteLengthFrom10cm() {
+            String value = "10cm";
+            float actual = CssUtils.ParseAbsoluteLength(value, CommonCssConstants.CM);
+            float expected = 283.46457f;
+            NUnit.Framework.Assert.AreEqual(expected, actual, 0);
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void ParseAbsoluteLengthFrom10in() {
+            String value = "10in";
+            float actual = CssUtils.ParseAbsoluteLength(value, CommonCssConstants.IN);
+            float expected = 720.0f;
+            NUnit.Framework.Assert.AreEqual(expected, actual, 0);
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void ParseAbsoluteLengthFrom10pt() {
+            String value = "10pt";
+            float actual = CssUtils.ParseAbsoluteLength(value, CommonCssConstants.PT);
+            float expected = 10.0f;
+            NUnit.Framework.Assert.AreEqual(expected, actual, 0);
+        }
+
+        [NUnit.Framework.Test]
+        [LogMessage(iText.StyledXmlParser.LogMessageConstant.UNKNOWN_ABSOLUTE_METRIC_LENGTH_PARSED, Count = 1)]
+        public virtual void ParseAbsoluteLengthFromUnknownType() {
+            String value = "10pateekes";
+            float actual = CssUtils.ParseAbsoluteLength(value, "pateekes");
+            float expected = 10.0f;
+            NUnit.Framework.Assert.AreEqual(expected, actual, 0);
+        }
+
         [NUnit.Framework.Test]
         public virtual void ValidateMetricValue() {
             NUnit.Framework.Assert.AreEqual(true, CssUtils.IsMetricValue("1px"));
