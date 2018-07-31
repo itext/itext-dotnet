54d825a63c9b1d2c9ebade07d6bea7911b1dff54
iText Software <development@itextpdf.com>
2018-07-31 16:53:30 +0300

    [RELEASE] 7.1.3-SNAPSHOT -> 7.1.4-SNAPSHOT

--- itext.tests/itext.barcodes.tests/Properties/AssemblyInfo.cs
+++ itext.tests/itext.barcodes.tests/Properties/AssemblyInfo.cs
@@ -14,9 +14,9 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("d015a3aa-613c-45d9-b908-7d47c4b613af")]
 
-[assembly: AssemblyVersion("7.1.3.0")]
-[assembly: AssemblyFileVersion("7.1.3.0")]
-[assembly: AssemblyInformationalVersion("7.1.3-SNAPSHOT")]
+[assembly: AssemblyVersion("7.1.4.0")]
+[assembly: AssemblyFileVersion("7.1.4.0")]
+[assembly: AssemblyInformationalVersion("7.1.4-SNAPSHOT")]
 
 #if !NETSTANDARD1_6
 [assembly: NUnit.Framework.Timeout(300000)]
--- itext/itext.io/Properties/AssemblyInfo.cs
+++ itext/itext.io/Properties/AssemblyInfo.cs
@@ -35,6 +35,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("39631ecb-1d39-4eb2-b775-37bd34cbf5a4")]
 
-[assembly: AssemblyVersion("7.1.3.0")]
-[assembly: AssemblyFileVersion("7.1.3.0")]
-[assembly: AssemblyInformationalVersion("7.1.3-SNAPSHOT")]
+[assembly: AssemblyVersion("7.1.4.0")]
+[assembly: AssemblyFileVersion("7.1.4.0")]
+[assembly: AssemblyInformationalVersion("7.1.4-SNAPSHOT")]
