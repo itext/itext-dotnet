5d7bbf9aadb7574269e9c6862a9e4a4be7fce7ee
Samuel Huylebroeck <samuel.huylebroeck@itextpdf.com>
2018-12-27 08:26:12 +0100

    Normalize PdfArray#toRectangle
    Will now return the rectangle defined by lower-left and upper-right corners instead of raw extractraction
    
    DEVSIX-2530
    
    Autoported commit.
    Original commit hash: [badd958c6]

--- itext/itext.kernel/itext/kernel/pdf/PdfArray.cs
+++ itext/itext.kernel/itext/kernel/pdf/PdfArray.cs
@@ -473,7 +473,16 @@ namespace iText.Kernel.Pdf {
                 float y1 = GetAsNumber(1).FloatValue();
                 float x2 = GetAsNumber(2).FloatValue();
                 float y2 = GetAsNumber(3).FloatValue();
-                return new Rectangle(x1, y1, x2 - x1, y2 - y1);
+                float llx;
+                float lly;
+                float urx;
+                float ury;
+                //Find the lower-left and upper-right of these 4 points
+                llx = Math.Min(x1, x2);
+                lly = Math.Min(y1, y2);
+                urx = Math.Max(x1, x2);
+                ury = Math.Max(y1, y2);
+                return new Rectangle(llx, lly, urx - llx, ury - lly);
             }
             catch (Exception e) {
                 throw new PdfException(PdfException.CannotConvertPdfArrayToRectanle, e, this);
