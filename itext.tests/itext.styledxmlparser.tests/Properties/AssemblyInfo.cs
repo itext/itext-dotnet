3b5dd7cb00b28879f6bdd8526e7ae34de02eb8af
Pavel Alay <pavel.alay@duallab.com>
2019-10-09 11:18:36 +0300

    Cleanup AssemblyInfo for sxp

--- itext.tests/itext.styledxmlparser.tests/Properties/AssemblyInfo.cs
+++ itext.tests/itext.styledxmlparser.tests/Properties/AssemblyInfo.cs
@@ -2,9 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("itext.StyledXmlParser.tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -14,27 +11,14 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("409b5131-4d0c-4e4b-9c7b-2407a8356ca4")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("7.1.9.0")]
 [assembly: AssemblyVersion("7.1.9.0")]
 [assembly: AssemblyFileVersion("7.1.9.0")]
 [assembly: AssemblyInformationalVersion("7.1.9-SNAPSHOT")]
+
 #if !NETSTANDARD1_6
 [assembly: NUnit.Framework.Timeout(300000)]
 #endif
--- itext/itext.styledxmlparser/Properties/AssemblyInfo.cs
+++ itext/itext.styledxmlparser/Properties/AssemblyInfo.cs
@@ -2,9 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("iText.StyledXmlParser")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -20,24 +17,10 @@ using System.Runtime.InteropServices;
                               "75bcdbcecb7caf1f0f4b6e7d013906ba60b66eb1c8298e4efb052caf6cece4bf1816" +
                               "902cc")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("8636f290-00df-403e-b841-e4bfd6d9ce7a")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("7.1.9.0")]
 [assembly: AssemblyVersion("7.1.9.0")]
 [assembly: AssemblyFileVersion("7.1.9.0")]
 [assembly: AssemblyInformationalVersion("7.1.9-SNAPSHOT")]
