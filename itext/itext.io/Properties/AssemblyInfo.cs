fce6b4be4e9c570eb6955c36fce1d0de337ce7c6
iText Software <development@itextpdf.com>
2020-01-16 14:32:13 +0300

    [RELEASE] 7.1.10-SNAPSHOT -> 7.1.10

--- itext/itext.io/Properties/AssemblyInfo.cs
+++ itext/itext.io/Properties/AssemblyInfo.cs
@@ -37,4 +37,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("7.1.10.0")]
 [assembly: AssemblyFileVersion("7.1.10.0")]
-[assembly: AssemblyInformationalVersion("7.1.10-SNAPSHOT")]
+[assembly: AssemblyInformationalVersion("7.1.10")]
