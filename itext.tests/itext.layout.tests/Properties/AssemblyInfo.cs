a3059fa7bd804a0d87e301cefd1e8cd8ba5ea017
iText Software <development@itextpdf.com>
2020-04-02 17:47:18 +0300

    [RELEASE] 7.1.11-SNAPSHOT -> 7.1.11

--- itext.tests/itext.layout.tests/Properties/AssemblyInfo.cs
+++ itext.tests/itext.layout.tests/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("7.1.11.0")]
 [assembly: AssemblyFileVersion("7.1.11.0")]
-[assembly: AssemblyInformationalVersion("7.1.11-SNAPSHOT")]
+[assembly: AssemblyInformationalVersion("7.1.11")]
 
 #if !NETSTANDARD1_6
 [assembly: NUnit.Framework.Timeout(600000)]
