41c6f6ff8f5d092e53a6fb5fa53bbdd08aeab4e0
Kate Ivanova <ekaterina.ivanova@duallab.com>
2020-05-06 21:16:42 +0200

    Update ToDo remarks in styled-xml-parser module
    DEVSIX-3976
    
    Autoported commit.
    Original commit hash: [51237bd9b]
    Manual files:
    styled-xml-parser/src/main/java/com/itextpdf/styledxmlparser/resolver/resource/UriResolver.java

--- itext/itext.styledxmlparser/itext/styledxmlparser/css/CssNestedAtRuleFactory.cs
+++ itext/itext.styledxmlparser/itext/styledxmlparser/css/CssNestedAtRuleFactory.cs
@@ -74,7 +74,7 @@ namespace iText.StyledXmlParser.Css {
             ruleDeclaration = ruleDeclaration.Trim();
             String ruleName = ExtractRuleNameFromDeclaration(ruleDeclaration);
             String ruleParameters = ruleDeclaration.Substring(ruleName.Length).Trim();
-            //TODO (RND-863) consider media rules in SVG
+            //TODO: DEVSIX-2263 consider media rules in SVG
             switch (ruleName) {
                 case CssRuleName.MEDIA: {
                     return new CssMediaRule(ruleParameters);
