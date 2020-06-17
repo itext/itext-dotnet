36ee1537a6219733c930eada18e5120bf72ac138
iText Software <development@itextpdf.com>
2020-06-17 14:06:12 +0300

    [RELEASE] Update dependency versions

--- itext/itext.kernel/Properties/AssemblyInfo.cs
+++ itext/itext.kernel/Properties/AssemblyInfo.cs
@@ -22,7 +22,7 @@ using Versions.Attributes;
 
 [assembly: Guid("be4c62cc-6495-47e1-ab00-05b1bfe331f6")]
 
-[assembly: KeyVersion("3.1.0.0")]
+[assembly: KeyVersion("3.1.1.0")]
 
 [assembly: AssemblyVersion("7.1.12.0")]
 [assembly: AssemblyFileVersion("7.1.12.0")]
