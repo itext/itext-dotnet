6283a44a082a78f809dccb3cc611f26bf71fe972
dzmitry.kachkou <dzmitry.kachkou@duallab.com>
2020-07-09 13:20:57 +0200

    Remove redundant comment
    DEVSIX-4093
    
    Autoported commit.
    Original commit hash: [2e4a41227]

--- itext/itext.pdfa/itext/pdfa/checker/PdfA1Checker.cs
+++ itext/itext.pdfa/itext/pdfa/checker/PdfA1Checker.cs
@@ -188,7 +188,6 @@ namespace iText.Pdfa.Checker {
         }
 
         public override void CheckXrefTable(PdfXrefTable xrefTable) {
-            // do not consider 'zero object' as it is not an indirect object
             if (xrefTable.GetCountOfIndirectObjects() > GetMaxNumberOfIndirectObjects()) {
                 throw new PdfAConformanceException(PdfAConformanceException.MAXIMUM_NUMBER_OF_INDIRECT_OBJECTS_EXCEEDED);
             }
