ec1a1becd8aa548a9201244924457e0436cdb442
iText Software <development@itextpdf.com>
2020-07-29 15:18:44 +0000

    Update autoported files
    9e5d9a67dfaca915c1f4cfd8315724ba569c6c80

--- itext.tests/itext.pdftest.tests/itext/test/LoggerHelperTest.cs
+++ itext.tests/itext.pdftest.tests/itext/test/LoggerHelperTest.cs
@@ -1,133 +1,118 @@
 /*
-    This file is part of the iText (R) project.
-    Copyright (c) 1998-2020 iText Group NV
-    Authors: iText Software.
+This file is part of the iText (R) project.
+Copyright (c) 1998-2020 iText Group NV
+Authors: iText Software.
 
-    This program is offered under a commercial and under the AGPL license.
-    For commercial licensing, contact us at https://itextpdf.com/sales.  For AGPL licensing, see below.
+This program is offered under a commercial and under the AGPL license.
+For commercial licensing, contact us at https://itextpdf.com/sales.  For AGPL licensing, see below.
 
-    AGPL licensing:
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+AGPL licensing:
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
 
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 using System;
-using NUnit.Framework;
 
 namespace iText.Test {
-    public class LoggerHelperTest : ExtendedITextTest
-    {
+    public class LoggerHelperTest : ExtendedITextTest {
         [NUnit.Framework.Test]
-        public void EqualsMessageByTemplate()
-        {
-            string pattern = "There might be a message: {0}";
-            string example = string.Format(pattern, "message");
-            Assert.IsTrue(LogListenerHelper.EqualsMessageByTemplate(example, pattern));
+        public virtual void EqualsMessageByTemplate() {
+            String pattern = "There might be a message: {0}";
+            String example = String.Format(pattern, "message");
+            NUnit.Framework.Assert.IsTrue(LogListenerHelper.EqualsMessageByTemplate(example, pattern));
         }
 
         [NUnit.Framework.Test]
-        public void EqualsMessageByTemplateWithEmptyParameter()
-        {
-            string pattern = "There might be a message: {0}";
-            string example = string.Format(pattern, "message");
-            Assert.IsTrue(LogListenerHelper.EqualsMessageByTemplate(example, pattern));
+        public virtual void EqualsMessageByTemplateWithEmptyParameter() {
+            String pattern = "There might be a message: {0}";
+            String example = String.Format(pattern, "message");
+            NUnit.Framework.Assert.IsTrue(LogListenerHelper.EqualsMessageByTemplate(example, pattern));
         }
 
         [NUnit.Framework.Test]
-        public void EqualsMessageByTemplateWithMultipleParameters()
-        {
-            string pattern = "There might be messages: {0} {1}";
-            string example = string.Format(pattern, "message1", "message2");
-            Assert.IsTrue(LogListenerHelper.EqualsMessageByTemplate(example, pattern));
+        public virtual void EqualsMessageByTemplateWithMultipleParameters() {
+            String pattern = "There might be messages: {0} {1}";
+            String example = String.Format(pattern, "message1", "message2");
+            NUnit.Framework.Assert.IsTrue(LogListenerHelper.EqualsMessageByTemplate(example, pattern));
         }
 
         [NUnit.Framework.Test]
-        public void EqualsMessageByTemplateWithQuotes()
-        {
-            string pattern = "There might be a message '': {0}";
-            string example = "There might be a message ': message";
-            Assert.IsTrue(LogListenerHelper.EqualsMessageByTemplate(example, pattern));
+        public virtual void EqualsMessageByTemplateWithQuotes() {
+            String pattern = "There might be a message '': {0}";
+            String example = "There might be a message ': message";
+            NUnit.Framework.Assert.IsTrue(LogListenerHelper.EqualsMessageByTemplate(example, pattern));
         }
 
         [NUnit.Framework.Test]
-        public void EqualsMessageByTemplateWithCyrillic()
-        {
-            string pattern = "There might be a cyrillic message: {0}";
-            string example = string.Format(pattern, "сообщение");
-            Assert.IsTrue(LogListenerHelper.EqualsMessageByTemplate(example, pattern));
+        public virtual void EqualsMessageByTemplateWithCyrillic() {
+            String pattern = "There might be a cyrillic message: {0}";
+            String example = String.Format(pattern, "сообщение");
+            NUnit.Framework.Assert.IsTrue(LogListenerHelper.EqualsMessageByTemplate(example, pattern));
         }
 
         [NUnit.Framework.Test]
-        public void EqualsMessageByTemplateWithAsterisks()
-        {
-            string pattern = "some text * *** {0}";
-            string example = string.Format(pattern, "message");
-            Assert.IsTrue(LogListenerHelper.EqualsMessageByTemplate(example, pattern));
+        public virtual void EqualsMessageByTemplateWithAsterisks() {
+            String pattern = "some text * *** {0}";
+            String example = String.Format(pattern, "message");
+            NUnit.Framework.Assert.IsTrue(LogListenerHelper.EqualsMessageByTemplate(example, pattern));
         }
 
         [NUnit.Framework.Test]
-        public void EqualsMessageByTemplateWithBrackets()
-        {
-            string pattern = "some text ( ) (0) ( {0}";
-            string example = string.Format(pattern, "message");
-            Assert.IsTrue(LogListenerHelper.EqualsMessageByTemplate(example, pattern));
+        public virtual void EqualsMessageByTemplateWithBrackets() {
+            String pattern = "some text ( ) (0) ( {0}";
+            String example = String.Format(pattern, "message");
+            NUnit.Framework.Assert.IsTrue(LogListenerHelper.EqualsMessageByTemplate(example, pattern));
         }
 
         [NUnit.Framework.Test]
-        public void EqualsMessageByTemplateWithSquareBrackets()
-        {
-            string pattern = "some text [ ] [0] [ {0}";
-            string example = string.Format(pattern, "message");
-            Assert.IsTrue(LogListenerHelper.EqualsMessageByTemplate(example, pattern));
+        public virtual void EqualsMessageByTemplateWithSquareBrackets() {
+            String pattern = "some text [ ] [0] [ {0}";
+            String example = String.Format(pattern, "message");
+            NUnit.Framework.Assert.IsTrue(LogListenerHelper.EqualsMessageByTemplate(example, pattern));
         }
 
         [NUnit.Framework.Test]
-        public void EqualsMessageByTemplateWithQuestionSign()
-        {
-            string pattern = "some text ? ??? .*? {0}";
-            string example = string.Format(pattern, "message");
-            Assert.IsTrue(LogListenerHelper.EqualsMessageByTemplate(example, pattern));
+        public virtual void EqualsMessageByTemplateWithQuestionSign() {
+            String pattern = "some text ? ??? .*? {0}";
+            String example = String.Format(pattern, "message");
+            NUnit.Framework.Assert.IsTrue(LogListenerHelper.EqualsMessageByTemplate(example, pattern));
         }
 
         [NUnit.Framework.Test]
-        public void EqualsMessageByTemplateWithDot()
-        {
-            string pattern = "some text . ... .* {0}";
-            string example = string.Format(pattern, "message");
-            Assert.IsTrue(LogListenerHelper.EqualsMessageByTemplate(example, pattern));
+        public virtual void EqualsMessageByTemplateWithDot() {
+            String pattern = "some text . ... .* {0}";
+            String example = String.Format(pattern, "message");
+            NUnit.Framework.Assert.IsTrue(LogListenerHelper.EqualsMessageByTemplate(example, pattern));
         }
 
         [NUnit.Framework.Test]
-        public void EqualsMessageByTemplateWithBraces()
-        {
-            string pattern = "some text {} {a} { {0}";
-            string example = "some text {} {a} { message";
-            Assert.IsTrue(LogListenerHelper.EqualsMessageByTemplate(example, pattern));
+        public virtual void EqualsMessageByTemplateWithBraces() {
+            String pattern = "some text {} {a} { {0}";
+            String example = "some text {} {a} { message";
+            NUnit.Framework.Assert.IsTrue(LogListenerHelper.EqualsMessageByTemplate(example, pattern));
         }
 
         [NUnit.Framework.Test]
-        public void EqualsComplicatedMessageByTemplate()
-        {
-            string pattern = "Not supported list style type ? {a} [b] . * (not working) {0}";
-            string example = "Not supported list style type ? {a} [b] . * (not working) *some phrase instead of template*";
-            Assert.IsTrue(LogListenerHelper.EqualsMessageByTemplate(example, pattern));
+        public virtual void EqualsComplicatedMessageByTemplate() {
+            String pattern = "Not supported list style type ? {a} [b] . * (not working) {0}";
+            String example = "Not supported list style type ? {a} [b] . * (not working) *some phrase instead of template*";
+            NUnit.Framework.Assert.IsTrue(LogListenerHelper.EqualsMessageByTemplate(example, pattern));
         }
 
         [NUnit.Framework.Test]
-        public void NotEqualsMessageByTemplate()
-        {
-            string pattern = "There must be a message: {0}";
-            string example = "There should be a message: message";
-            Assert.IsFalse(LogListenerHelper.EqualsMessageByTemplate(example, pattern));
+        public virtual void NotEqualsMessageByTemplate() {
+            String pattern = "There might be a message: {0}";
+            String example = "There should be a message: message";
+            NUnit.Framework.Assert.IsFalse(LogListenerHelper.EqualsMessageByTemplate(example, pattern));
         }
     }
 }
