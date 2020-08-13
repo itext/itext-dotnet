cf560896db7af6b42c07665dd33cca4b867b854c
iText Software <development@itextpdf.com>
2020-08-13 12:17:25 +0000

    Add missing copyright headers
    Autoported commit.
    Original commit hash: [1a37cf7bb]

--- itext.tests/itext.kernel.tests/itext/kernel/pdf/canvas/parser/PdfCanvasProcessorUnitTest.cs
+++ itext.tests/itext.kernel.tests/itext/kernel/pdf/canvas/parser/PdfCanvasProcessorUnitTest.cs
@@ -1,3 +1,25 @@
+/*
+This file is part of the iText (R) project.
+Copyright (c) 1998-2020 iText Group NV
+Authors: iText Software.
+
+This program is offered under a commercial and under the AGPL license.
+For commercial licensing, contact us at https://itextpdf.com/sales.  For AGPL licensing, see below.
+
+AGPL licensing:
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 using iText.IO.Util;
 using iText.Kernel.Pdf;
 using iText.Kernel.Pdf.Canvas.Parser.Listener;
@@ -7,14 +29,14 @@ namespace iText.Kernel.Pdf.Canvas.Parser {
     public class PdfCanvasProcessorUnitTest : ExtendedITextTest {
         [NUnit.Framework.Test]
         public virtual void BeginMarkerContentOperatorTest() {
-            PdfCanvasProcessor processor = new _PdfCanvasProcessor_20(new FilteredEventListener());
+            PdfCanvasProcessor processor = new _PdfCanvasProcessor_42(new FilteredEventListener());
             IContentOperator contentOperator = processor.RegisterContentOperator("BMC", null);
             processor.RegisterContentOperator("BMC", contentOperator);
             contentOperator.Invoke(processor, null, JavaCollectionsUtil.SingletonList((PdfObject)null));
         }
 
-        private sealed class _PdfCanvasProcessor_20 : PdfCanvasProcessor {
-            public _PdfCanvasProcessor_20(IEventListener baseArg1)
+        private sealed class _PdfCanvasProcessor_42 : PdfCanvasProcessor {
+            public _PdfCanvasProcessor_42(IEventListener baseArg1)
                 : base(baseArg1) {
             }
 
