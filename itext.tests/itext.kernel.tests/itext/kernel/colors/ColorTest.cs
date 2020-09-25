4c6e16baaddae84c0c0fa91b506dbbcb4d69d292
Yauheni Borbut <yauheni.borbut@duallab.com>
2020-09-25 11:08:22 +0000

    Improve coverage for kernel/colors/Color
    DEVSIX-4518
    
    Autoported commit.
    Original commit hash: [26cd63555]

---
+++ itext.tests/itext.kernel.tests/itext/kernel/colors/ColorTest.cs
@@ -0,0 +1,369 @@
+/*
+This file is part of the iText (R) project.
+Copyright (c) 1998-2020 iText Group NV
+Authors: iText Software.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License version 3
+as published by the Free Software Foundation with the addition of the
+following permission added to Section 15 as permitted in Section 7(a):
+FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY
+ITEXT GROUP. ITEXT GROUP DISCLAIMS THE WARRANTY OF NON INFRINGEMENT
+OF THIRD PARTY RIGHTS
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License
+along with this program; if not, see http://www.gnu.org/licenses or write to
+the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+Boston, MA, 02110-1301 USA, or download the license from the following URL:
+http://itextpdf.com/terms-of-use/
+
+The interactive user interfaces in modified source and object code versions
+of this program must display Appropriate Legal Notices, as required under
+Section 5 of the GNU Affero General Public License.
+
+In accordance with Section 7(b) of the GNU Affero General Public License,
+a covered work must retain the producer line in every PDF that is created
+or manipulated using iText.
+
+You can be released from the requirements of the license by purchasing
+a commercial license. Buying such a license is mandatory as soon as you
+develop commercial activities involving the iText software without
+disclosing the source code of your own applications.
+These activities include: offering paid services to customers as an ASP,
+serving PDFs on the fly in a web application, shipping iText with a closed
+source product.
+
+For more information, please contact iText Software Corp. at this
+address: sales@itextpdf.com
+*/
+using iText.Kernel;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Colorspace;
+using iText.Test;
+
+namespace iText.Kernel.Colors {
+    public class ColorTest : ExtendedITextTest {
+        private const float EPS = 1e-4f;
+
+        [NUnit.Framework.Test]
+        public virtual void ConvertCmykToRgbTest() {
+            DeviceCmyk cmyk = new DeviceCmyk(0, 0, 0, 0);
+            DeviceRgb rgb = new DeviceRgb(255, 255, 255);
+            NUnit.Framework.Assert.AreEqual(rgb.colorValue, Color.ConvertCmykToRgb(cmyk).colorValue);
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void ConvertRgbToCmykTest() {
+            DeviceCmyk cmyk = new DeviceCmyk(0, 0, 0, 0);
+            DeviceRgb rgb = new DeviceRgb(255, 255, 255);
+            NUnit.Framework.Assert.AreEqual(cmyk.colorValue, Color.ConvertRgbToCmyk(rgb).colorValue);
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void SetColorValueIncorrectComponentsNumberTest() {
+            NUnit.Framework.Assert.That(() =>  {
+                float[] colorValues = new float[] { 0.0f, 0.5f, 0.1f };
+                Color color = Color.MakeColor(PdfColorSpace.MakeColorSpace(PdfName.DeviceRGB), colorValues);
+                color.SetColorValue(new float[] { 0.1f, 0.2f });
+            }
+            , NUnit.Framework.Throws.InstanceOf<PdfException>().With.Message.EqualTo(PdfException.IncorrectNumberOfComponents))
+;
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void EqualsAndHashCodeTest() {
+            float[] colorValues = new float[] { 0.0f, 0.5f, 0.1f };
+            Color color1 = Color.MakeColor(PdfColorSpace.MakeColorSpace(PdfName.DeviceRGB), colorValues);
+            Color color2 = Color.MakeColor(PdfColorSpace.MakeColorSpace(PdfName.DeviceRGB), colorValues);
+            bool result = color1.Equals(color2);
+            NUnit.Framework.Assert.IsTrue(result);
+            NUnit.Framework.Assert.AreEqual(color1.GetHashCode(), color2.GetHashCode());
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void EqualsAndHashCodeNullColorSpacesTest() {
+            float[] colorValues = new float[] { 0.0f, 0.5f, 0.1f };
+            Color color1 = new Color(null, colorValues);
+            Color color2 = new Color(null, colorValues);
+            bool result = color1.Equals(color2);
+            NUnit.Framework.Assert.IsTrue(result);
+            NUnit.Framework.Assert.AreEqual(color1.GetHashCode(), color2.GetHashCode());
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void EqualsAndHashCodeNullColorValuesTest() {
+            Color color1 = new Color(PdfColorSpace.MakeColorSpace(PdfName.DeviceRGB), null);
+            Color color2 = new Color(PdfColorSpace.MakeColorSpace(PdfName.DeviceRGB), null);
+            bool result = color1.Equals(color2);
+            NUnit.Framework.Assert.IsTrue(result);
+            NUnit.Framework.Assert.AreEqual(color1.GetHashCode(), color2.GetHashCode());
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void NotEqualsAndHashCodeDifferentColorSpacesTest() {
+            float[] colorValues = new float[] { 0.0f, 0.5f, 0.1f };
+            Color color1 = Color.MakeColor(PdfColorSpace.MakeColorSpace(PdfName.DeviceRGB), colorValues);
+            Color color2 = Color.MakeColor(PdfColorSpace.MakeColorSpace(PdfName.DeviceGray), colorValues);
+            bool result = color1.Equals(color2);
+            NUnit.Framework.Assert.IsFalse(result);
+            NUnit.Framework.Assert.AreNotEqual(color1.GetHashCode(), color2.GetHashCode());
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void NotEqualsNullObjectTest() {
+            float[] colorValues = new float[] { 0.0f, 0.5f, 0.1f };
+            Color color1 = Color.MakeColor(PdfColorSpace.MakeColorSpace(PdfName.DeviceRGB), colorValues);
+            bool result = color1.Equals(null);
+            NUnit.Framework.Assert.IsFalse(result);
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void NotEqualsDifferentClassesTest() {
+            float[] colorValues = new float[] { 0.0f, 0.5f, 0.1f };
+            Color color1 = Color.MakeColor(PdfColorSpace.MakeColorSpace(PdfName.DeviceRGB), colorValues);
+            DeviceCmyk cmyk = new DeviceCmyk(0, 0, 0, 0);
+            bool result = color1.Equals(cmyk);
+            NUnit.Framework.Assert.IsFalse(result);
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void NullColorSpaceTest() {
+            NUnit.Framework.Assert.That(() =>  {
+                float[] colorValues = new float[] { 0.0f, 0.5f, 0.1f };
+                Color color = Color.MakeColor(null, colorValues);
+            }
+            , NUnit.Framework.Throws.InstanceOf<PdfException>().With.Message.EqualTo("Unknown color space."))
+;
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void MakeDeviceGrayNullColorValuesTest() {
+            PdfColorSpace colorSpace = PdfColorSpace.MakeColorSpace(PdfName.DeviceGray);
+            Color color = Color.MakeColor(colorSpace);
+            NUnit.Framework.Assert.IsTrue(color is DeviceGray);
+            NUnit.Framework.Assert.AreEqual(new float[] { 0.0f }, color.GetColorValue());
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void MakeDeviceGrayTest() {
+            float[] colorValues = new float[] { 0.7f, 0.5f, 0.1f };
+            PdfColorSpace colorSpace = PdfColorSpace.MakeColorSpace(PdfName.DeviceGray);
+            Color color = Color.MakeColor(colorSpace, colorValues);
+            NUnit.Framework.Assert.IsTrue(color is DeviceGray);
+            NUnit.Framework.Assert.AreEqual(new float[] { 0.7f }, color.GetColorValue());
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void MakeDeviceCmykNullColorValuesTest() {
+            PdfColorSpace colorSpace = PdfColorSpace.MakeColorSpace(PdfName.DeviceCMYK);
+            Color color = Color.MakeColor(colorSpace);
+            NUnit.Framework.Assert.IsTrue(color is DeviceCmyk);
+            NUnit.Framework.Assert.AreEqual(new float[] { 0.0f, 0.0f, 0.0f, 1.0f }, color.GetColorValue());
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void MakeDeviceCmykTest() {
+            float[] colorValues = new float[] { 0.7f, 0.5f, 0.1f, 0.3f };
+            PdfColorSpace colorSpace = PdfColorSpace.MakeColorSpace(PdfName.DeviceCMYK);
+            Color color = Color.MakeColor(colorSpace, colorValues);
+            NUnit.Framework.Assert.IsTrue(color is DeviceCmyk);
+            NUnit.Framework.Assert.AreEqual(colorValues, color.GetColorValue());
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void UnknownDeviceCsTest() {
+            NUnit.Framework.Assert.That(() =>  {
+                Color color = Color.MakeColor(new ColorTest.CustomDeviceCs(null));
+            }
+            , NUnit.Framework.Throws.InstanceOf<PdfException>().With.Message.EqualTo("Unknown color space."))
+;
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void MakeCalGrayNullColorValuesTest() {
+            PdfDictionary dictionary = new PdfDictionary();
+            dictionary.Put(PdfName.Gamma, new PdfNumber(2.2));
+            PdfArray calGray = new PdfArray();
+            calGray.Add(PdfName.CalGray);
+            calGray.Add(dictionary);
+            PdfColorSpace colorSpace = PdfColorSpace.MakeColorSpace(calGray);
+            Color color = Color.MakeColor(colorSpace);
+            NUnit.Framework.Assert.IsTrue(color is CalGray);
+            NUnit.Framework.Assert.AreEqual(new float[] { 0.0f }, color.GetColorValue());
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void MakeCalGrayTest() {
+            PdfDictionary dictionary = new PdfDictionary();
+            dictionary.Put(PdfName.Gamma, new PdfNumber(2.2));
+            PdfArray calGray = new PdfArray();
+            calGray.Add(PdfName.CalGray);
+            calGray.Add(dictionary);
+            PdfColorSpace colorSpace = PdfColorSpace.MakeColorSpace(calGray);
+            float[] colorValues = new float[] { 0.7f, 0.5f, 0.1f };
+            Color color = Color.MakeColor(colorSpace, colorValues);
+            NUnit.Framework.Assert.IsTrue(color is CalGray);
+            NUnit.Framework.Assert.AreEqual(new float[] { 0.7f }, color.GetColorValue());
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void MakeCalRgbNullColorValuesTest() {
+            PdfDictionary dictionary = new PdfDictionary();
+            dictionary.Put(PdfName.Gamma, new PdfNumber(2.2));
+            PdfArray calRgb = new PdfArray();
+            calRgb.Add(PdfName.CalRGB);
+            calRgb.Add(dictionary);
+            PdfColorSpace colorSpace = PdfColorSpace.MakeColorSpace(calRgb);
+            Color color = Color.MakeColor(colorSpace);
+            NUnit.Framework.Assert.IsTrue(color is CalRgb);
+            NUnit.Framework.Assert.AreEqual(new float[] { 0.0f, 0.0f, 0.0f }, color.GetColorValue());
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void MakeCalRgbTest() {
+            PdfDictionary dictionary = new PdfDictionary();
+            dictionary.Put(PdfName.Gamma, new PdfNumber(2.2));
+            PdfArray calRgb = new PdfArray();
+            calRgb.Add(PdfName.CalRGB);
+            calRgb.Add(dictionary);
+            PdfColorSpace colorSpace = PdfColorSpace.MakeColorSpace(calRgb);
+            float[] colorValues = new float[] { 0.7f, 0.5f, 0.1f };
+            Color color = Color.MakeColor(colorSpace, colorValues);
+            NUnit.Framework.Assert.IsTrue(color is CalRgb);
+            NUnit.Framework.Assert.AreEqual(colorValues, color.GetColorValue());
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void MakeLabNullColorValuesTest() {
+            PdfDictionary dictionary = new PdfDictionary();
+            dictionary.Put(PdfName.Gamma, new PdfNumber(2.2));
+            PdfArray calLab = new PdfArray();
+            calLab.Add(PdfName.Lab);
+            calLab.Add(dictionary);
+            PdfColorSpace colorSpace = PdfColorSpace.MakeColorSpace(calLab);
+            Color color = Color.MakeColor(colorSpace);
+            NUnit.Framework.Assert.IsTrue(color is Lab);
+            NUnit.Framework.Assert.AreEqual(new float[] { 0.0f, 0.0f, 0.0f }, color.GetColorValue());
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void MakeLabTest() {
+            PdfDictionary dictionary = new PdfDictionary();
+            dictionary.Put(PdfName.Gamma, new PdfNumber(2.2));
+            PdfArray calLab = new PdfArray();
+            calLab.Add(PdfName.Lab);
+            calLab.Add(dictionary);
+            PdfColorSpace colorSpace = PdfColorSpace.MakeColorSpace(calLab);
+            float[] colorValues = new float[] { 0.7f, 0.5f, 0.1f };
+            Color color = Color.MakeColor(colorSpace, colorValues);
+            NUnit.Framework.Assert.IsTrue(color is Lab);
+            NUnit.Framework.Assert.AreEqual(colorValues, color.GetColorValue());
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void UnknownCieBasedCsTest() {
+            NUnit.Framework.Assert.That(() =>  {
+                Color color = Color.MakeColor(new ColorTest.CustomPdfCieBasedCs(new PdfArray()));
+            }
+            , NUnit.Framework.Throws.InstanceOf<PdfException>().With.Message.EqualTo("Unknown color space."))
+;
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void MakeDeviceNNullColorValuesTest() {
+            PdfArray deviceN = new PdfArray();
+            deviceN.Add(PdfName.DeviceN);
+            deviceN.Add(new PdfArray());
+            deviceN.Add(null);
+            deviceN.Add(null);
+            PdfColorSpace colorSpace = PdfColorSpace.MakeColorSpace(deviceN);
+            Color color = Color.MakeColor(colorSpace);
+            NUnit.Framework.Assert.IsTrue(color is DeviceN);
+            NUnit.Framework.Assert.AreEqual(new float[] {  }, color.GetColorValue());
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void MakeDeviceNTest() {
+            PdfArray deviceN = new PdfArray();
+            deviceN.Add(PdfName.DeviceN);
+            deviceN.Add(new PdfArray());
+            deviceN.Add(null);
+            deviceN.Add(null);
+            PdfColorSpace colorSpace = PdfColorSpace.MakeColorSpace(deviceN);
+            float[] colorValues = new float[] { 0.7f, 0.5f, 0.1f };
+            Color color = Color.MakeColor(colorSpace, colorValues);
+            NUnit.Framework.Assert.IsTrue(color is DeviceN);
+            NUnit.Framework.Assert.AreEqual(colorValues, color.GetColorValue());
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void MakeIndexedNullColorValuesTest() {
+            PdfArray indexed = new PdfArray();
+            indexed.Add(PdfName.Indexed);
+            indexed.Add(new PdfArray());
+            indexed.Add(null);
+            indexed.Add(null);
+            PdfColorSpace colorSpace = PdfColorSpace.MakeColorSpace(indexed);
+            Color color = Color.MakeColor(colorSpace);
+            NUnit.Framework.Assert.IsTrue(color is Indexed);
+            NUnit.Framework.Assert.AreEqual(new float[] { 0.0f }, color.GetColorValue());
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void MakeIndexedTest() {
+            PdfArray indexed = new PdfArray();
+            indexed.Add(PdfName.Indexed);
+            indexed.Add(new PdfArray());
+            indexed.Add(null);
+            indexed.Add(null);
+            PdfColorSpace colorSpace = PdfColorSpace.MakeColorSpace(indexed);
+            float[] colorValues = new float[] { 1.0f, 0.5f, 0.1f };
+            Color color = Color.MakeColor(colorSpace, colorValues);
+            NUnit.Framework.Assert.IsTrue(color is Indexed);
+            NUnit.Framework.Assert.AreEqual(new float[] { 1f }, color.GetColorValue());
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void UnknownSpecialCsTest() {
+            NUnit.Framework.Assert.That(() =>  {
+                Color color = Color.MakeColor(new ColorTest.CustomPdfSpecialCs(new PdfArray()));
+            }
+            , NUnit.Framework.Throws.InstanceOf<PdfException>().With.Message.EqualTo("Unknown color space."))
+;
+        }
+
+        private class CustomDeviceCs : PdfDeviceCs {
+            public CustomDeviceCs(PdfName pdfObject)
+                : base(pdfObject) {
+            }
+
+            public override int GetNumberOfComponents() {
+                return 0;
+            }
+        }
+
+        private class CustomPdfCieBasedCs : PdfCieBasedCs {
+            public CustomPdfCieBasedCs(PdfArray pdfObject)
+                : base(pdfObject) {
+            }
+
+            public override int GetNumberOfComponents() {
+                return 0;
+            }
+        }
+
+        private class CustomPdfSpecialCs : PdfSpecialCs {
+            public CustomPdfSpecialCs(PdfArray pdfObject)
+                : base(pdfObject) {
+            }
+
+            public override int GetNumberOfComponents() {
+                return 0;
+            }
+        }
+    }
+}
