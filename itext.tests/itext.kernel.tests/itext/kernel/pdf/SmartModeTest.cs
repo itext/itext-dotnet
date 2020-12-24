cda9b246fb178330589cbc22ea6d22fb172c8e9b
Kate Ivanova <ekaterina.ivanova@duallab.com>
2020-12-24 18:13:54 +0100

    Add new tests to SmartModeTests class
    Replace tests from samples-internal module, add new one
    
    DEVSIX-3904, DEVSIX-403
    
    Autoported commit.
    Original commit hash: [48253f599]

--- itext.tests/itext.kernel.tests/itext/kernel/pdf/SmartModeTest.cs
+++ itext.tests/itext.kernel.tests/itext/kernel/pdf/SmartModeTest.cs
@@ -203,5 +203,253 @@ namespace iText.Kernel.Pdf {
             origPdf.Close();
             NUnit.Framework.Assert.IsNull(new CompareTool().CompareByContent(destFile, cmpFile, destinationFolder));
         }
+
+        [NUnit.Framework.Test]
+        public virtual void SmartModeSameImageResourcesTest() {
+            String srcFile = sourceFolder + "sameImageResources.pdf";
+            String outFile = destinationFolder + "smartModeSameImageResources.pdf";
+            String cmpFile = sourceFolder + "cmp_smartModeSameImageResources.pdf";
+            using (PdfDocument newDoc = new PdfDocument(new PdfWriter(outFile).SetSmartMode(true))) {
+                using (PdfDocument srcDoc = new PdfDocument(new PdfReader(srcFile))) {
+                    srcDoc.CopyPagesTo(1, srcDoc.GetNumberOfPages(), newDoc);
+                }
+                PdfIndirectReference page1ImgRes = newDoc.GetPage(1).GetPdfObject().GetAsDictionary(PdfName.Resources).GetAsDictionary
+                    (PdfName.XObject).GetAsStream(new PdfName("Im0")).GetIndirectReference();
+                PdfIndirectReference page2ImgRes = newDoc.GetPage(2).GetPdfObject().GetAsDictionary(PdfName.Resources).GetAsDictionary
+                    (PdfName.XObject).GetAsStream(new PdfName("Im0")).GetIndirectReference();
+                NUnit.Framework.Assert.AreEqual(page1ImgRes, page2ImgRes);
+            }
+            NUnit.Framework.Assert.IsNull(new CompareTool().CompareByContent(outFile, cmpFile, destinationFolder));
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void SmartModeSameColorSpaceResourcesTest() {
+            String srcFile = sourceFolder + "colorSpaceResource.pdf";
+            String outFile = destinationFolder + "smartModeSameColorSpaceResources.pdf";
+            String cmpFile = sourceFolder + "cmp_smartModeSameColorSpaceResources.pdf";
+            using (PdfDocument newDoc = new PdfDocument(new PdfWriter(outFile).SetSmartMode(true))) {
+                for (int i = 0; i < 2; i++) {
+                    using (PdfDocument srcDoc = new PdfDocument(new PdfReader(srcFile))) {
+                        srcDoc.CopyPagesTo(1, srcDoc.GetNumberOfPages(), newDoc);
+                    }
+                }
+                PdfObject page1CsRes = newDoc.GetPage(1).GetPdfObject().GetAsDictionary(PdfName.Resources).GetAsDictionary
+                    (PdfName.XObject).Get(new PdfName("Im0")).GetIndirectReference();
+                PdfObject page2CsRes = newDoc.GetPage(2).GetPdfObject().GetAsDictionary(PdfName.Resources).GetAsDictionary
+                    (PdfName.XObject).Get(new PdfName("Im0")).GetIndirectReference();
+                // It's expected that indirect arrays are not processed by smart mode.
+                // Smart mode only merges duplicate dictionaries and streams.
+                NUnit.Framework.Assert.AreEqual(page1CsRes, page2CsRes);
+                PdfIndirectReference page1CsStm = newDoc.GetPage(1).GetPdfObject().GetAsDictionary(PdfName.Resources).GetAsDictionary
+                    (PdfName.ColorSpace).GetAsArray(new PdfName("CS0")).GetAsStream(1).GetIndirectReference();
+                PdfIndirectReference page2CsStm = newDoc.GetPage(2).GetPdfObject().GetAsDictionary(PdfName.Resources).GetAsDictionary
+                    (PdfName.ColorSpace).GetAsArray(new PdfName("CS0")).GetAsStream(1).GetIndirectReference();
+                NUnit.Framework.Assert.AreEqual(page1CsStm, page2CsStm);
+            }
+            NUnit.Framework.Assert.IsNull(new CompareTool().CompareByContent(outFile, cmpFile, destinationFolder));
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void SmartModeSameExtGStateResourcesTest() {
+            String srcFile = sourceFolder + "extGStateResource.pdf";
+            String outFile = destinationFolder + "smartModeSameExtGStateResources.pdf";
+            String cmpFile = sourceFolder + "cmp_smartModeSameExtGStateResources.pdf";
+            using (PdfDocument newDoc = new PdfDocument(new PdfWriter(outFile).SetSmartMode(true))) {
+                for (int i = 0; i < 2; i++) {
+                    using (PdfDocument srcDoc = new PdfDocument(new PdfReader(srcFile))) {
+                        srcDoc.CopyPagesTo(1, srcDoc.GetNumberOfPages(), newDoc);
+                    }
+                }
+                PdfIndirectReference page1GsRes = newDoc.GetPage(1).GetPdfObject().GetAsDictionary(PdfName.Resources).GetAsDictionary
+                    (PdfName.ExtGState).GetAsDictionary(new PdfName("Gs1")).GetIndirectReference();
+                PdfIndirectReference page2GsRes = newDoc.GetPage(2).GetPdfObject().GetAsDictionary(PdfName.Resources).GetAsDictionary
+                    (PdfName.ExtGState).GetAsDictionary(new PdfName("Gs1")).GetIndirectReference();
+                NUnit.Framework.Assert.AreEqual(page1GsRes, page2GsRes);
+            }
+            NUnit.Framework.Assert.IsNull(new CompareTool().CompareByContent(outFile, cmpFile, destinationFolder));
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void SmartModeCopyingInTaggedPdfTest() {
+            String srcFile = sourceFolder + "simpleTaggedDocument.pdf";
+            String dstFile = destinationFolder + "smartModeCopyingInTaggedPdf.pdf";
+            String cmpFile = sourceFolder + "cmp_smartModeCopyingInTaggedPdf.pdf";
+            using (PdfDocument pdfDest = new PdfDocument(new PdfWriter(dstFile, new WriterProperties().UseSmartMode())
+                )) {
+                pdfDest.SetTagged();
+                using (PdfDocument pdfSrc = new PdfDocument(new PdfReader(srcFile))) {
+                    pdfSrc.CopyPagesTo(1, pdfSrc.GetNumberOfPages(), pdfDest);
+                }
+                NUnit.Framework.Assert.IsNotNull(pdfDest.GetStructTreeRoot().GetKidsObject().GetAsDictionary(0).GetAsArray
+                    (PdfName.K).GetAsDictionary(0).Get(PdfName.K));
+            }
+            NUnit.Framework.Assert.IsNull(new CompareTool().CompareByContent(dstFile, cmpFile, destinationFolder));
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void SmartModeCopyingInPdfWithIdenticalPagesTaggedTest() {
+            String srcFile = sourceFolder + "docWithAllPagesIdenticalTagged.pdf";
+            String dstFile = destinationFolder + "smartModeCopyingInPdfWithIdenticalPagesTagged.pdf";
+            using (PdfDocument pdfDest = new PdfDocument(new PdfWriter(dstFile, new WriterProperties().UseSmartMode())
+                )) {
+                pdfDest.SetTagged();
+                using (PdfDocument pdfSrc = new PdfDocument(new PdfReader(srcFile))) {
+                    pdfSrc.CopyPagesTo(1, pdfSrc.GetNumberOfPages(), pdfDest);
+                }
+                PdfIndirectReference expectedPageObj = pdfDest.GetPage(1).GetPdfObject().GetIndirectReference();
+                PdfIndirectReference expectedContStm = pdfDest.GetPage(1).GetPdfObject().GetAsStream(PdfName.Contents).GetIndirectReference
+                    ();
+                for (int i = 2; i <= 10; i++) {
+                    PdfIndirectReference pageObj = pdfDest.GetPage(i).GetPdfObject().GetIndirectReference();
+                    NUnit.Framework.Assert.AreNotEqual(expectedPageObj, pageObj);
+                    PdfIndirectReference pageContentStm = pdfDest.GetPage(i).GetPdfObject().GetAsStream(PdfName.Contents).GetIndirectReference
+                        ();
+                    NUnit.Framework.Assert.AreEqual(expectedContStm, pageContentStm);
+                }
+            }
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void SmartModeCopyingInPdfWithIdenticalPagesTest() {
+            String srcFile = sourceFolder + "docWithAllPagesIdenticalNotTagged.pdf";
+            String dstFile = destinationFolder + "smartModeCopyingInPdfWithIdenticalPagesNotTagged.pdf";
+            using (PdfDocument pdfDest = new PdfDocument(new PdfWriter(dstFile, new WriterProperties().UseSmartMode())
+                )) {
+                using (PdfDocument pdfSrc = new PdfDocument(new PdfReader(srcFile))) {
+                    pdfSrc.CopyPagesTo(1, pdfSrc.GetNumberOfPages(), pdfDest);
+                }
+                PdfIndirectReference expectedPageObj = pdfDest.GetPage(1).GetPdfObject().GetIndirectReference();
+                PdfIndirectReference expectedContStm = pdfDest.GetPage(1).GetPdfObject().GetAsStream(PdfName.Contents).GetIndirectReference
+                    ();
+                for (int i = 2; i <= 10; i++) {
+                    PdfIndirectReference pageObj = pdfDest.GetPage(i).GetPdfObject().GetIndirectReference();
+                    NUnit.Framework.Assert.AreNotEqual(expectedPageObj, pageObj);
+                    PdfIndirectReference pageContentStm = pdfDest.GetPage(i).GetPdfObject().GetAsStream(PdfName.Contents).GetIndirectReference
+                        ();
+                    NUnit.Framework.Assert.AreEqual(expectedContStm, pageContentStm);
+                }
+            }
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void SmartModeCopyingInPdfSamePagesDifferentXObjectsTest() {
+            String srcFile = sourceFolder + "identicalPagesDifferentXObjects.pdf";
+            String dstFile = destinationFolder + "smartModeCopyingInPdfSamePagesDifferentXObjects.pdf";
+            using (PdfDocument pdfDest = new PdfDocument(new PdfWriter(dstFile, new WriterProperties().UseSmartMode())
+                )) {
+                using (PdfDocument pdfSrc = new PdfDocument(new PdfReader(srcFile))) {
+                    pdfSrc.CopyPagesTo(1, pdfSrc.GetNumberOfPages(), pdfDest);
+                }
+                PdfIndirectReference expectedImgRes = pdfDest.GetPage(1).GetPdfObject().GetAsDictionary(PdfName.Resources)
+                    .GetAsDictionary(PdfName.XObject).GetAsStream(new PdfName("Im1")).GetIndirectReference();
+                for (int i = 2; i <= 99; i++) {
+                    PdfIndirectReference pagesImgRes = pdfDest.GetPage(i).GetPdfObject().GetAsDictionary(PdfName.Resources).GetAsDictionary
+                        (PdfName.XObject).GetAsStream(new PdfName("Im1")).GetIndirectReference();
+                    NUnit.Framework.Assert.AreEqual(expectedImgRes, pagesImgRes);
+                }
+            }
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void SmartCopyingOfArrayWithStringsTest() {
+            String srcFile = sourceFolder + "keyValueStructure.pdf";
+            String dstFile = destinationFolder + "smartCopyingOfArrayWithStrings.pdf";
+            using (PdfDocument pdfDest = new PdfDocument(new PdfWriter(dstFile, new WriterProperties().UseSmartMode())
+                )) {
+                using (PdfDocument pdfSrc = new PdfDocument(new PdfReader(srcFile))) {
+                    pdfSrc.CopyPagesTo(1, pdfSrc.GetNumberOfPages(), pdfDest);
+                }
+                PdfIndirectReference key1Ref = pdfDest.GetPage(1).GetPdfObject().GetAsDictionary(new PdfName("Key1")).GetIndirectReference
+                    ();
+                PdfIndirectReference key2Ref = pdfDest.GetPage(2).GetPdfObject().GetAsDictionary(new PdfName("Key2")).GetIndirectReference
+                    ();
+                // Currently smart mode copying doesn't affect any other objects except streams and dictionaries
+                NUnit.Framework.Assert.AreNotEqual(key1Ref, key2Ref);
+            }
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void SmartCopyingOfNestedIndirectDictionariesTest() {
+            String srcFile = sourceFolder + "nestedIndirectDictionaries.pdf";
+            String dstFile = destinationFolder + "smartCopyingOfNestedIndirectDictionariesTest.pdf";
+            using (PdfDocument pdfDest = new PdfDocument(new PdfWriter(dstFile, new WriterProperties().UseSmartMode())
+                )) {
+                using (PdfDocument pdfSrc = new PdfDocument(new PdfReader(srcFile))) {
+                    pdfSrc.CopyPagesTo(1, pdfSrc.GetNumberOfPages(), pdfDest);
+                }
+                PdfObject key1Page1Ref = pdfDest.GetPage(1).GetPdfObject().GetAsDictionary(new PdfName("Key1")).GetIndirectReference
+                    ();
+                PdfIndirectReference key1Page2Ref = pdfDest.GetPage(2).GetPdfObject().GetAsDictionary(new PdfName("Key1"))
+                    .GetIndirectReference();
+                PdfIndirectReference key2Page1Ref = pdfDest.GetPage(1).GetPdfObject().GetAsDictionary(new PdfName("Key1"))
+                    .GetAsDictionary(new PdfName("Key2")).GetIndirectReference();
+                PdfIndirectReference key2Page2Ref = pdfDest.GetPage(2).GetPdfObject().GetAsDictionary(new PdfName("Key1"))
+                    .GetAsDictionary(new PdfName("Key2")).GetIndirectReference();
+                PdfIndirectReference key3Page1Ref = pdfDest.GetPage(1).GetPdfObject().GetAsDictionary(new PdfName("Key1"))
+                    .GetAsDictionary(new PdfName("Key2")).GetAsArray(new PdfName("Key3")).GetIndirectReference();
+                PdfIndirectReference key3Page2Ref = pdfDest.GetPage(2).GetPdfObject().GetAsDictionary(new PdfName("Key1"))
+                    .GetAsDictionary(new PdfName("Key2")).GetAsArray(new PdfName("Key3")).GetIndirectReference();
+                // Currently smart mode copying doesn't affect any other objects except streams and dictionaries
+                NUnit.Framework.Assert.AreEqual(key1Page1Ref, key1Page2Ref);
+                NUnit.Framework.Assert.AreEqual(key2Page1Ref, key2Page2Ref);
+                NUnit.Framework.Assert.AreEqual(key3Page1Ref, key3Page2Ref);
+            }
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void SmartModeSeparatedOutlinesCopyingTest() {
+            String dstFile = destinationFolder + "smartModeSeparatedOutlinesCopying.pdf";
+            String cmpFile = sourceFolder + "cmp_smartModeSeparatedOutlinesCopying.pdf";
+            String[] srcFiles = new String[] { sourceFolder + "separatedOutlinesCopying.pdf", sourceFolder + "separatedOutlinesCopying.pdf"
+                 };
+            using (PdfDocument outputDoc = new PdfDocument(new PdfWriter(dstFile, new WriterProperties().UseSmartMode(
+                )))) {
+                outputDoc.InitializeOutlines();
+                foreach (String srcFile in srcFiles) {
+                    PdfDocument sourceDoc = new PdfDocument(new PdfReader(srcFile));
+                    sourceDoc.CopyPagesTo(1, sourceDoc.GetNumberOfPages(), outputDoc);
+                    sourceDoc.Close();
+                }
+            }
+            NUnit.Framework.Assert.IsNull(new CompareTool().CompareByContent(dstFile, cmpFile, destinationFolder));
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void SmartModeCopyingInPdfWithLinksOnOnePageTest() {
+            String srcFile = sourceFolder + "identical100PagesDiffObjectsLinksOnOnePage.pdf";
+            String dstFile = destinationFolder + "smartModeCopyingInPdfWithLinksOnOnePage.pdf";
+            using (PdfDocument pdfDest = new PdfDocument(new PdfWriter(dstFile, new WriterProperties().UseSmartMode())
+                )) {
+                using (PdfDocument pdfSrc = new PdfDocument(new PdfReader(srcFile))) {
+                    pdfSrc.CopyPagesTo(1, pdfSrc.GetNumberOfPages(), pdfDest);
+                }
+                PdfIndirectReference expectedImgRes = pdfDest.GetPage(1).GetPdfObject().GetAsDictionary(PdfName.Resources)
+                    .GetAsDictionary(PdfName.XObject).GetAsStream(new PdfName("Im1")).GetIndirectReference();
+                for (int i = 2; i <= 99; i++) {
+                    PdfIndirectReference pagesImgRes = pdfDest.GetPage(i).GetPdfObject().GetAsDictionary(PdfName.Resources).GetAsDictionary
+                        (PdfName.XObject).GetAsStream(new PdfName("Im1")).GetIndirectReference();
+                    NUnit.Framework.Assert.AreEqual(expectedImgRes, pagesImgRes);
+                }
+            }
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void SmartModeCopyingInPdfWithDiffImagesTest() {
+            String srcFile = sourceFolder + "docWithDifferentImages.pdf";
+            String dstFile = destinationFolder + "smartModeCopyingInPdfWithDiffImages.pdf";
+            using (PdfDocument pdfDest = new PdfDocument(new PdfWriter(dstFile, new WriterProperties().UseSmartMode())
+                )) {
+                using (PdfDocument pdfSrc = new PdfDocument(new PdfReader(srcFile))) {
+                    pdfSrc.CopyPagesTo(1, pdfSrc.GetNumberOfPages(), pdfDest);
+                }
+                PdfIndirectReference expectedImgRes = pdfDest.GetPage(1).GetPdfObject().GetAsDictionary(PdfName.Resources)
+                    .GetAsDictionary(PdfName.XObject).GetAsStream(new PdfName("Im1")).GetIndirectReference();
+                for (int i = 2; i <= 99; i++) {
+                    PdfIndirectReference pagesImgRes = pdfDest.GetPage(i).GetPdfObject().GetAsDictionary(PdfName.Resources).GetAsDictionary
+                        (PdfName.XObject).GetAsStream(new PdfName("Im1")).GetIndirectReference();
+                    NUnit.Framework.Assert.AreNotEqual(expectedImgRes, pagesImgRes);
+                }
+            }
+        }
     }
 }
