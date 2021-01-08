06a58389fd24d59e5c6dffc29b722b7e01c7d101
iText Software <development@itextpdf.com>
2021-01-08 08:01:01 +0000

    Add missing copyright headers and update copyright year
    DEVSIX-4930
    
    Autoported commit.
    Original commit hash: [25c8f4d5]

--- itext.tests/itext.pdfa.tests/Properties/AssemblyInfo.cs
+++ itext.tests/itext.pdfa.tests/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("iText Group NV")]
 [assembly: AssemblyProduct("iText")]
-[assembly: AssemblyCopyright("Copyright (c) 1998-2020 iText Group NV")]
+[assembly: AssemblyCopyright("Copyright (c) 1998-2021 iText Group NV")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
