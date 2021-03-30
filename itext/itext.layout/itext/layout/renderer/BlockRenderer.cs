540677977efda1b9547944a80b4b94915546fab3
Eugene Bochilo <eugene.bochilo@duallab.com>
2021-03-30 17:59:46 +0000

    Get rid of new javadoc warnings
    DEVSIX-5226
    
    Autoported commit.
    Original commit hash: [8f959ac34]

--- itext/itext.layout/itext/layout/renderer/BlockRenderer.cs
+++ itext/itext.layout/itext/layout/renderer/BlockRenderer.cs
@@ -908,6 +908,15 @@ namespace iText.Layout.Renderer {
             }
         }
 
+        /// <summary>Decreases parentBBox to the size of borders, paddings and margins.</summary>
+        /// <param name="parentBBox">
+        /// 
+        /// <see cref="iText.Kernel.Geom.Rectangle"/>
+        /// to be decreased
+        /// </param>
+        /// <param name="borders">the border values to decrease parentBBox</param>
+        /// <param name="paddings">the padding values to decrease parentBBox</param>
+        /// <returns>the difference between previous and current parentBBox's</returns>
         [System.ObsoleteAttribute(@"Need to be removed in next major release.")]
         protected internal virtual float ApplyBordersPaddingsMargins(Rectangle parentBBox, Border[] borders, UnitValue
             [] paddings) {
