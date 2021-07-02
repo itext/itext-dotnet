13bc5180e8f7c4777297a3bb77ea240fbc400d6a
Nikita Kovaliov <nikita.kovaliov@duallab.com>
2021-07-02 11:02:14 +0000

    Migrate SigningTest to a separate classes
    DEVSIX-5398
    
    Autoported commit.
    Original commit hash: [45f3d6118]

---
+++ itext.tests/itext.sign.tests/itext/signatures/sign/EncryptedSigningTest.cs
@@ -0,0 +1,100 @@
+/*
+This file is part of the iText (R) project.
+Copyright (c) 1998-2021 iText Group NV
+Authors: iText Software.
+
+This program is offered under a commercial and under the AGPL license.
+For commercial licensing, contact us at https://itextpdf.com/sales.  For AGPL licensing, see below.
+
+AGPL licensing:
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+using System;
+using System.IO;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.X509;
+using iText.Kernel;
+using iText.Kernel.Pdf;
+using iText.Signatures;
+using iText.Signatures.Testutils;
+using iText.Test;
+using iText.Test.Signutils;
+
+namespace iText.Signatures.Sign {
+    public class EncryptedSigningTest : ExtendedITextTest {
+        private static readonly String SOURCE_FOLDER = iText.Test.TestUtil.GetParentProjectDirectory(NUnit.Framework.TestContext
+            .CurrentContext.TestDirectory) + "/resources/itext/signatures/sign" + "/EncryptedSigningTest/";
+
+        private static readonly String DESTINATION_FOLDER = NUnit.Framework.TestContext.CurrentContext.TestDirectory
+             + "/test/itext/signatures/sign/EncryptedSigningTest/";
+
+        private static readonly String CERTS_SRC = iText.Test.TestUtil.GetParentProjectDirectory(NUnit.Framework.TestContext
+            .CurrentContext.TestDirectory) + "/resources/itext/signatures/certs/";
+
+        private static readonly char[] PASSWORD = "testpass".ToCharArray();
+
+        private X509Certificate[] chain;
+
+        private ICipherParameters pk;
+
+        [NUnit.Framework.OneTimeSetUp]
+        public static void Before() {
+            CreateOrClearDestinationFolder(DESTINATION_FOLDER);
+        }
+
+        [NUnit.Framework.SetUp]
+        public virtual void Init() {
+            pk = Pkcs12FileHelper.ReadFirstKey(CERTS_SRC + "signCertRsa01.p12", PASSWORD, PASSWORD);
+            chain = Pkcs12FileHelper.ReadFirstChain(CERTS_SRC + "signCertRsa01.p12", PASSWORD);
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void SignEncryptedPdfTest() {
+            String srcFile = SOURCE_FOLDER + "encrypted.pdf";
+            String cmpPdf = SOURCE_FOLDER + "cmp_signedEncrypted.pdf";
+            String outPdf = DESTINATION_FOLDER + "signedEncrypted.pdf";
+            String fieldName = "Signature1";
+            byte[] ownerPass = "World".GetBytes();
+            PdfReader reader = new PdfReader(srcFile, new ReaderProperties().SetPassword(ownerPass));
+            PdfSigner signer = new PdfSigner(reader, new FileStream(outPdf, FileMode.Create), new StampingProperties()
+                .UseAppendMode());
+            // Creating the appearance
+            PdfSignatureAppearance appearance = signer.GetSignatureAppearance().SetReason("Test1").SetLocation("TestCity"
+                );
+            signer.SetFieldName(fieldName);
+            // Creating the signature
+            IExternalSignature pks = new PrivateKeySignature(pk, DigestAlgorithms.SHA256);
+            signer.SignDetached(pks, chain, null, null, null, 0, PdfSigner.CryptoStandard.CADES);
+            //TODO DEVSIX-5637 Improve SignaturesCompareTool#compareSignatures to check encrypted pdf
+            NUnit.Framework.Assert.AreEqual(PdfException.BadUserPassword, SignaturesCompareTool.CompareSignatures(outPdf
+                , cmpPdf));
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void SignCertificateSecurityPdfTest() {
+            String srcFile = SOURCE_FOLDER + "signCertificateSecurityPdf.pdf";
+            String cmpPdf = SOURCE_FOLDER + "cmp_signCertificateSecurityPdf.pdf";
+            String outPdf = DESTINATION_FOLDER + "signCertificateSecurityPdf.pdf";
+            PdfReader reader = new PdfReader(srcFile, new ReaderProperties().SetPublicKeySecurityParams(chain[0], pk));
+            PdfSigner signer = new PdfSigner(reader, new FileStream(outPdf, FileMode.Create), new StampingProperties()
+                .UseAppendMode());
+            // Creating the signature
+            IExternalSignature pks = new PrivateKeySignature(pk, DigestAlgorithms.SHA256);
+            signer.SignDetached(pks, chain, null, null, null, 0, PdfSigner.CryptoStandard.CADES);
+            //TODO DEVSIX-5637 Improve SignaturesCompareTool#compareSignatures to check encrypted pdf
+            NUnit.Framework.Assert.AreEqual(PdfException.CertificateIsNotProvidedDocumentIsEncryptedWithPublicKeyCertificate
+                , SignaturesCompareTool.CompareSignatures(outPdf, cmpPdf));
+        }
+    }
+}
---
+++ itext.tests/itext.sign.tests/itext/signatures/sign/TaggedPdfSigningTest.cs
@@ -0,0 +1,134 @@
+/*
+This file is part of the iText (R) project.
+Copyright (c) 1998-2021 iText Group NV
+Authors: iText Software.
+
+This program is offered under a commercial and under the AGPL license.
+For commercial licensing, contact us at https://itextpdf.com/sales.  For AGPL licensing, see below.
+
+AGPL licensing:
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.X509;
+using iText.IO.Util;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Kernel.Utils;
+using iText.Signatures;
+using iText.Signatures.Testutils;
+using iText.Test;
+using iText.Test.Signutils;
+
+namespace iText.Signatures.Sign {
+    public class TaggedPdfSigningTest : ExtendedITextTest {
+        private static readonly String SOURCE_FOLDER = iText.Test.TestUtil.GetParentProjectDirectory(NUnit.Framework.TestContext
+            .CurrentContext.TestDirectory) + "/resources/itext/signatures/sign/TaggedPdfSigningTest/";
+
+        private static readonly String DESTINATION_FOLDER = NUnit.Framework.TestContext.CurrentContext.TestDirectory
+             + "/test/itext/signatures/sign/TaggedPdfSigningTest/";
+
+        private static readonly String CERTS_SRC = iText.Test.TestUtil.GetParentProjectDirectory(NUnit.Framework.TestContext
+            .CurrentContext.TestDirectory) + "/resources/itext/signatures/certs/";
+
+        private static readonly char[] PASSWORD = "testpass".ToCharArray();
+
+        private X509Certificate[] chain;
+
+        private ICipherParameters pk;
+
+        [NUnit.Framework.OneTimeSetUp]
+        public static void Before() {
+            CreateOrClearDestinationFolder(DESTINATION_FOLDER);
+        }
+
+        [NUnit.Framework.SetUp]
+        public virtual void Init() {
+            pk = Pkcs12FileHelper.ReadFirstKey(CERTS_SRC + "signCertRsa01.p12", PASSWORD, PASSWORD);
+            chain = Pkcs12FileHelper.ReadFirstChain(CERTS_SRC + "signCertRsa01.p12", PASSWORD);
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void SigningTaggedDocumentTest() {
+            String srcFile = SOURCE_FOLDER + "simpleTaggedDocument.pdf";
+            String cmpPdf = SOURCE_FOLDER + "cmp_simpleTaggedDocument.pdf";
+            String outPdf = DESTINATION_FOLDER + "simpleTaggedDocument.pdf";
+            Rectangle rect = new Rectangle(36, 648, 200, 100);
+            String fieldName = "Signature1";
+            Sign(srcFile, fieldName, outPdf, chain, pk, DigestAlgorithms.SHA256, PdfSigner.CryptoStandard.CADES, "Test 1"
+                , "TestCity", rect, false, false);
+            NUnit.Framework.Assert.IsNull(new CompareTool().CompareVisually(outPdf, cmpPdf, DESTINATION_FOLDER, "diff_"
+                , GetTestMap(rect)));
+            NUnit.Framework.Assert.IsNull(new CompareTool().CompareTagStructures(outPdf, cmpPdf));
+            NUnit.Framework.Assert.IsNull(SignaturesCompareTool.CompareSignatures(outPdf, cmpPdf));
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void SigningTaggedDocumentAppendModeTest() {
+            String srcFile = SOURCE_FOLDER + "simpleTaggedDocumentAppendMode.pdf";
+            String cmpPdf = SOURCE_FOLDER + "cmp_simpleTaggedDocumentAppendMode.pdf";
+            String outPdf = DESTINATION_FOLDER + "signedTaggedDocumentAppendMode.pdf";
+            Rectangle rect = new Rectangle(36, 648, 200, 100);
+            String fieldName = "Signature1";
+            Sign(srcFile, fieldName, outPdf, chain, pk, DigestAlgorithms.SHA256, PdfSigner.CryptoStandard.CADES, "Test 1"
+                , "TestCity", rect, false, true);
+            NUnit.Framework.Assert.IsNull(new CompareTool().CompareVisually(outPdf, cmpPdf, DESTINATION_FOLDER, "diff_"
+                , GetTestMap(rect)));
+            NUnit.Framework.Assert.IsNull(new CompareTool().CompareTagStructures(outPdf, cmpPdf));
+            NUnit.Framework.Assert.IsNull(SignaturesCompareTool.CompareSignatures(outPdf, cmpPdf));
+        }
+
+        protected internal virtual void Sign(String src, String name, String dest, X509Certificate[] chain, ICipherParameters
+             pk, String digestAlgorithm, PdfSigner.CryptoStandard subfilter, String reason, String location, Rectangle
+             rectangleForNewField, bool setReuseAppearance, bool isAppendMode) {
+            Sign(src, name, dest, chain, pk, digestAlgorithm, subfilter, reason, location, rectangleForNewField, setReuseAppearance
+                , isAppendMode, PdfSigner.NOT_CERTIFIED, null);
+        }
+
+        protected internal virtual void Sign(String src, String name, String dest, X509Certificate[] chain, ICipherParameters
+             pk, String digestAlgorithm, PdfSigner.CryptoStandard subfilter, String reason, String location, Rectangle
+             rectangleForNewField, bool setReuseAppearance, bool isAppendMode, int certificationLevel, float? fontSize
+            ) {
+            PdfReader reader = new PdfReader(src);
+            StampingProperties properties = new StampingProperties();
+            if (isAppendMode) {
+                properties.UseAppendMode();
+            }
+            PdfSigner signer = new PdfSigner(reader, new FileStream(dest, FileMode.Create), properties);
+            signer.SetCertificationLevel(certificationLevel);
+            // Creating the appearance
+            PdfSignatureAppearance appearance = signer.GetSignatureAppearance().SetReason(reason).SetLocation(location
+                ).SetReuseAppearance(setReuseAppearance);
+            if (rectangleForNewField != null) {
+                appearance.SetPageRect(rectangleForNewField);
+            }
+            if (fontSize != null) {
+                appearance.SetLayer2FontSize((float)fontSize);
+            }
+            signer.SetFieldName(name);
+            // Creating the signature
+            IExternalSignature pks = new PrivateKeySignature(pk, digestAlgorithm);
+            signer.SignDetached(pks, chain, null, null, null, 0, subfilter);
+        }
+
+        private static IDictionary<int, IList<Rectangle>> GetTestMap(Rectangle ignoredArea) {
+            IDictionary<int, IList<Rectangle>> result = new Dictionary<int, IList<Rectangle>>();
+            result.Put(1, JavaUtil.ArraysAsList(ignoredArea));
+            return result;
+        }
+    }
+}
