bd187d9234375b618a9cf2c2528d27c20f036a57
Uladzimir Asipchuk <vladimir.osipchuk@duallab.com>
2021-08-04 20:49:47 +0000

    Improve input processing for ImageMagick and Ghostscript
    DEVSIX-5638
    
    Autoported commit.
    Original commit hash: [6b61c858a]
    Manual files:
    io/src/main/java/com/itextpdf/io/util/FileUtil.java
    io/src/test/java/com/itextpdf/io/util/StandardOutUtil.java

--- itext.tests/itext.io.tests/itext/io/util/GhostscriptHelperTest.cs
+++ itext.tests/itext.io.tests/itext/io/util/GhostscriptHelperTest.cs
@@ -53,6 +53,10 @@ namespace iText.IO.Util {
         private static readonly String DESTINATION_FOLDER = NUnit.Framework.TestContext.CurrentContext.TestDirectory
              + "/test/itext/io/GhostscriptHelperTest/";
 
+        // In some of the test we will check whether Ghostscript has printed its help message to the console.
+        // The value of this threshold should be definitely less than the length of the help message.
+        private const int SYSTEM_OUT_LENGTH_LIMIT = 400;
+
         [NUnit.Framework.SetUp]
         public virtual void SetUp() {
             CreateOrClearDestinationFolder(DESTINATION_FOLDER);
@@ -99,11 +103,11 @@ namespace iText.IO.Util {
         [NUnit.Framework.Test]
         public virtual void RunGhostScriptIncorrectParams() {
             String inputPdf = SOURCE_FOLDER + "imageHandlerUtilTest.pdf";
-            String exceptionMessage = "GhostScript failed for " + inputPdf;
+            String invalidPageList = "q@W";
+            String exceptionMessage = "Invalid page list: " + invalidPageList;
             GhostscriptHelper ghostscriptHelper = new GhostscriptHelper();
-            Exception e = NUnit.Framework.Assert.Catch(typeof(GhostscriptHelper.GhostscriptExecutionException), () => 
-                ghostscriptHelper.RunGhostScriptImageGeneration(inputPdf, DESTINATION_FOLDER, "outputPageImage.png", "q@W"
-                ));
+            Exception e = NUnit.Framework.Assert.Catch(typeof(ArgumentException), () => ghostscriptHelper.RunGhostScriptImageGeneration
+                (inputPdf, DESTINATION_FOLDER, "outputPageImage.png", invalidPageList));
             NUnit.Framework.Assert.AreEqual(exceptionMessage, e.Message);
         }
 
@@ -111,16 +115,16 @@ namespace iText.IO.Util {
         public virtual void RunGhostScriptTestForSpecificPage() {
             String inputPdf = SOURCE_FOLDER + "imageHandlerUtilTest.pdf";
             GhostscriptHelper ghostscriptHelper = new GhostscriptHelper();
-            ghostscriptHelper.RunGhostScriptImageGeneration(inputPdf, DESTINATION_FOLDER, "specificPage.png", "1");
+            ghostscriptHelper.RunGhostScriptImageGeneration(inputPdf, DESTINATION_FOLDER, "specificPage", "1");
             NUnit.Framework.Assert.AreEqual(1, FileUtil.ListFilesInDirectory(DESTINATION_FOLDER, true).Length);
-            NUnit.Framework.Assert.IsTrue(FileUtil.FileExists(DESTINATION_FOLDER + "specificPage.png"));
+            NUnit.Framework.Assert.IsTrue(FileUtil.FileExists(DESTINATION_FOLDER + "specificPage-001.png"));
         }
 
         [NUnit.Framework.Test]
         public virtual void RunGhostScriptTestForSeveralSpecificPages() {
             String inputPdf = SOURCE_FOLDER + "imageHandlerUtilTest.pdf";
             GhostscriptHelper ghostscriptHelper = new GhostscriptHelper();
-            String imageFileName = new FileInfo(inputPdf).Name + "_severalSpecificPages-%03d.png";
+            String imageFileName = new FileInfo(inputPdf).Name + "_severalSpecificPages";
             ghostscriptHelper.RunGhostScriptImageGeneration(inputPdf, DESTINATION_FOLDER, imageFileName, "1,3");
             NUnit.Framework.Assert.AreEqual(2, FileUtil.ListFilesInDirectory(DESTINATION_FOLDER, true).Length);
             NUnit.Framework.Assert.IsTrue(FileUtil.FileExists(DESTINATION_FOLDER + "imageHandlerUtilTest.pdf_severalSpecificPages-001.png"
@@ -133,7 +137,7 @@ namespace iText.IO.Util {
         public virtual void RunGhostScriptTestForAllPages() {
             String inputPdf = SOURCE_FOLDER + "imageHandlerUtilTest.pdf";
             GhostscriptHelper ghostscriptHelper = new GhostscriptHelper();
-            String imageFileName = new FileInfo(inputPdf).Name + "_allPages-%03d.png";
+            String imageFileName = new FileInfo(inputPdf).Name + "_allPages";
             ghostscriptHelper.RunGhostScriptImageGeneration(inputPdf, DESTINATION_FOLDER, imageFileName);
             NUnit.Framework.Assert.AreEqual(3, FileUtil.ListFilesInDirectory(DESTINATION_FOLDER, true).Length);
             NUnit.Framework.Assert.IsTrue(FileUtil.FileExists(DESTINATION_FOLDER + "imageHandlerUtilTest.pdf_allPages-001.png"
@@ -148,8 +152,6 @@ namespace iText.IO.Util {
         public virtual void DSaferParamInGhostScriptHelperTest() {
             String input = SOURCE_FOLDER + "unsafePostScript.ps";
             String outputName = "unsafePostScript.png";
-            String maliciousResult1 = DESTINATION_FOLDER + "output1.txt";
-            String maliciousResult2 = DESTINATION_FOLDER + "output2.txt";
             try {
                 GhostscriptHelper ghostscriptHelper = new GhostscriptHelper();
                 ghostscriptHelper.RunGhostScriptImageGeneration(input, DESTINATION_FOLDER, outputName);
@@ -159,23 +161,87 @@ namespace iText.IO.Util {
                      + "This is expected for some environments and ghostscript versions. " + "We assert only the absence of malicious script result (created file).\n"
                     );
             }
+            // If we had not set -dSAFER option, the following files would be created
+            String maliciousResult1 = DESTINATION_FOLDER + "output1.txt";
+            String maliciousResult2 = DESTINATION_FOLDER + "output2.txt";
             NUnit.Framework.Assert.IsFalse(FileUtil.FileExists(maliciousResult1));
             NUnit.Framework.Assert.IsFalse(FileUtil.FileExists(maliciousResult2));
         }
 
         [NUnit.Framework.Test]
         public virtual void GhostScriptImageGenerationTest() {
-            String filename = "resultantImage.png";
+            String name = "resultantImage";
+            String filename = name + ".png";
             String psFile = SOURCE_FOLDER + "simple.ps";
-            String resultantImage = DESTINATION_FOLDER + filename;
+            String resultantImage = DESTINATION_FOLDER + name + "-001.png";
             String cmpResultantImage = SOURCE_FOLDER + "cmp_" + filename;
             String diff = DESTINATION_FOLDER + "diff_" + filename;
             GhostscriptHelper ghostscriptHelper = new GhostscriptHelper();
-            ghostscriptHelper.RunGhostScriptImageGeneration(psFile, DESTINATION_FOLDER, filename);
+            ghostscriptHelper.RunGhostScriptImageGeneration(psFile, DESTINATION_FOLDER, name);
             NUnit.Framework.Assert.IsTrue(FileUtil.FileExists(resultantImage));
             ImageMagickHelper imageMagickHelper = new ImageMagickHelper();
             NUnit.Framework.Assert.IsTrue(imageMagickHelper.RunImageMagickImageCompare(resultantImage, cmpResultantImage
                 , diff));
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void PdfCallsHelpTest() {
+            // Previously this test printed help message. Now an exception should be thrown.
+            String inputPdf = SOURCE_FOLDER + "../test.pdf -h";
+            String outputImagePattern = "image";
+            GhostscriptHelper ghostscriptHelper = new GhostscriptHelper();
+            // In .NET the type of the thrown exception is different, therefore we just check here that
+            // any exception has been thrown.
+            NUnit.Framework.Assert.Catch(typeof(Exception), () => ghostscriptHelper.RunGhostScriptImageGeneration(inputPdf
+                , DESTINATION_FOLDER, outputImagePattern));
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void OutputImageCallsHelpTest() {
+            String inputPdf = SOURCE_FOLDER + "../test.pdf";
+            String outputImagePattern = "justSomeText \" -h";
+            GhostscriptHelper ghostscriptHelper = new GhostscriptHelper();
+            Object storedPrintStream = System.Console.Out;
+            MemoryStream baos = new MemoryStream();
+            try {
+                System.Console.SetOut(new FormattingStreamWriter(baos));
+                ghostscriptHelper.RunGhostScriptImageGeneration(inputPdf, DESTINATION_FOLDER, outputImagePattern);
+            }
+            catch (Exception) {
+            }
+            finally {
+                // This test fails on Windows, but works on Linux. So our goal is not to check
+                // whether an exception was thrown, but whether there is the help message in the output
+                System.Console.Out.Flush();
+                StandardOutUtil.RestoreStandardOut(storedPrintStream);
+                NUnit.Framework.Assert.IsTrue(baos.ToArray().Length < SYSTEM_OUT_LENGTH_LIMIT);
+                baos.Dispose();
+            }
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void PageListCallsHelpTest() {
+            // Previously this test printed help message. Now an exception should be thrown.
+            String inputPdf = SOURCE_FOLDER + "../test.pdf";
+            String outputImagePattern = "justSomeText";
+            String pageList = "1 -h";
+            GhostscriptHelper ghostscriptHelper = new GhostscriptHelper();
+            // In .NET the type of the thrown exception is different, therefore we just check here that
+            // any exception has been thrown.
+            NUnit.Framework.Assert.Catch(typeof(Exception), () => ghostscriptHelper.RunGhostScriptImageGeneration(inputPdf
+                , DESTINATION_FOLDER, outputImagePattern, pageList));
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void NonExistingDestinationFolder() {
+            String inputPdf = SOURCE_FOLDER + "../test.pdf";
+            String outputImagePattern = "justSomeText";
+            String destinationFolder = "notExistingFolder";
+            GhostscriptHelper ghostscriptHelper = new GhostscriptHelper();
+            // In .NET the type of the thrown exception is different, therefore we just check here that
+            // any exception has been thrown.
+            NUnit.Framework.Assert.Catch(typeof(Exception), () => ghostscriptHelper.RunGhostScriptImageGeneration(inputPdf
+                , destinationFolder, outputImagePattern));
         }
     }
 }
