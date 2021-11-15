a0a9b6a5e0eed0ae9e15a8306a64c02cf2bd15ab
Dmitry Chubrick <dima.chubrick@duallab.com>
2021-11-15 09:33:36 +0000

    Add JavaDocs to AccessibilityProperties class
    DEVSIX-6058
    
    Autoported commit.
    Original commit hash: [f5c6ea5d6]

--- itext/itext.kernel/itext/kernel/pdf/tagutils/AccessibilityProperties.cs
+++ itext/itext.kernel/itext/kernel/pdf/tagutils/AccessibilityProperties.cs
@@ -46,95 +46,292 @@ using iText.Commons.Utils;
 using iText.Kernel.Pdf.Tagging;
 
 namespace iText.Kernel.Pdf.Tagutils {
+    /// <summary>
+    /// The accessibility properties are used to define properties of
+    /// <see cref="iText.Kernel.Pdf.Tagging.PdfStructElem">structure elements</see>
+    /// in Tagged PDF documents via
+    /// <see cref="TagTreePointer"/>
+    /// API.
+    /// </summary>
     public abstract class AccessibilityProperties {
+        /// <summary>Gets the role of element.</summary>
+        /// <remarks>
+        /// Gets the role of element.
+        /// <para />
+        /// See also
+        /// <see cref="iText.Kernel.Pdf.Tagging.StandardRoles"/>.
+        /// </remarks>
+        /// <returns>the role</returns>
         public virtual String GetRole() {
             return null;
         }
 
+        /// <summary>Sets the role of element.</summary>
+        /// <remarks>
+        /// Sets the role of element.
+        /// <para />
+        /// See also
+        /// <see cref="iText.Kernel.Pdf.Tagging.StandardRoles"/>.
+        /// </remarks>
+        /// <param name="role">the role to be set</param>
+        /// <returns>
+        /// this
+        /// <see cref="AccessibilityProperties"/>
+        /// instance
+        /// </returns>
         public virtual AccessibilityProperties SetRole(String role) {
             return this;
         }
 
+        /// <summary>Gets the language identifier of element.</summary>
+        /// <remarks>
+        /// Gets the language identifier of element. Should be in format xy-ZK (for example en-US).
+        /// <para />
+        /// For more information see PDF Specification ISO 32000-1 section 14.9.2.
+        /// </remarks>
+        /// <returns>the language</returns>
         public virtual String GetLanguage() {
             return null;
         }
 
+        /// <summary>Sets the language identifier of element.</summary>
+        /// <remarks>
+        /// Sets the language identifier of element. Should be in format xy-ZK (for example en-US).
+        /// <para />
+        /// For more information see PDF Specification ISO 32000-1 section 14.9.2.
+        /// </remarks>
+        /// <param name="language">the language to be set</param>
+        /// <returns>
+        /// this
+        /// <see cref="AccessibilityProperties"/>
+        /// instance
+        /// </returns>
         public virtual AccessibilityProperties SetLanguage(String language) {
             return this;
         }
 
+        /// <summary>Gets the actual text of element.</summary>
+        /// <returns>the actual text</returns>
         public virtual String GetActualText() {
             return null;
         }
 
+        /// <summary>Sets the actual text of element.</summary>
+        /// <param name="actualText">the actual text to be set</param>
+        /// <returns>
+        /// this
+        /// <see cref="AccessibilityProperties"/>
+        /// instance
+        /// </returns>
         public virtual AccessibilityProperties SetActualText(String actualText) {
             return this;
         }
 
+        /// <summary>Gets the alternate description of element.</summary>
+        /// <returns>the alternate description</returns>
         public virtual String GetAlternateDescription() {
             return null;
         }
 
+        /// <summary>Sets the alternate description of element.</summary>
+        /// <param name="alternateDescription">the alternation description to be set</param>
+        /// <returns>
+        /// this
+        /// <see cref="AccessibilityProperties"/>
+        /// instance
+        /// </returns>
         public virtual AccessibilityProperties SetAlternateDescription(String alternateDescription) {
             return this;
         }
 
+        /// <summary>Gets the expansion of element.</summary>
+        /// <remarks>
+        /// Gets the expansion of element.
+        /// <para />
+        /// Expansion it is the expanded form of an abbreviation of structure element.
+        /// </remarks>
+        /// <returns>the expansion</returns>
         public virtual String GetExpansion() {
             return null;
         }
 
+        /// <summary>Sets the expansion of element.</summary>
+        /// <remarks>
+        /// Sets the expansion of element.
+        /// <para />
+        /// Expansion it is the expanded form of an abbreviation of structure element.
+        /// </remarks>
+        /// <param name="expansion">the expansion to be set</param>
+        /// <returns>
+        /// this
+        /// <see cref="AccessibilityProperties"/>
+        /// instance
+        /// </returns>
         public virtual AccessibilityProperties SetExpansion(String expansion) {
             return this;
         }
 
+        /// <summary>Gets the phoneme of element.</summary>
+        /// <remarks>
+        /// Gets the phoneme of element.
+        /// <para />
+        /// For more information see
+        /// <see cref="iText.Kernel.Pdf.Tagging.PdfStructElem.SetPhoneme(iText.Kernel.Pdf.PdfString)"/>.
+        /// </remarks>
+        /// <returns>the phoneme</returns>
         public virtual String GetPhoneme() {
             return null;
         }
 
+        /// <summary>Sets the phoneme of element.</summary>
+        /// <remarks>
+        /// Sets the phoneme of element.
+        /// <para />
+        /// For more information see
+        /// <see cref="iText.Kernel.Pdf.Tagging.PdfStructElem.SetPhoneme(iText.Kernel.Pdf.PdfString)"/>.
+        /// </remarks>
+        /// <param name="phoneme">the phoneme to be set</param>
+        /// <returns>
+        /// this
+        /// <see cref="AccessibilityProperties"/>
+        /// instance
+        /// </returns>
         public virtual AccessibilityProperties SetPhoneme(String phoneme) {
             return this;
         }
 
+        /// <summary>Gets the phonetic alphabet of element.</summary>
+        /// <remarks>
+        /// Gets the phonetic alphabet of element.
+        /// <para />
+        /// For more information see
+        /// <see cref="iText.Kernel.Pdf.Tagging.PdfStructElem.SetPhoneticAlphabet(iText.Kernel.Pdf.PdfName)"/>.
+        /// </remarks>
+        /// <returns>the phonetic alphabet</returns>
         public virtual String GetPhoneticAlphabet() {
             return null;
         }
 
+        /// <summary>Sets the phonetic alphabet of element.</summary>
+        /// <remarks>
+        /// Sets the phonetic alphabet of element.
+        /// <para />
+        /// For more information see
+        /// <see cref="iText.Kernel.Pdf.Tagging.PdfStructElem.SetPhoneticAlphabet(iText.Kernel.Pdf.PdfName)"/>.
+        /// </remarks>
+        /// <param name="phoneticAlphabet">the phonetic alphabet to be set</param>
+        /// <returns>
+        /// this
+        /// <see cref="AccessibilityProperties"/>
+        /// instance
+        /// </returns>
         public virtual AccessibilityProperties SetPhoneticAlphabet(String phoneticAlphabet) {
             return this;
         }
 
+        /// <summary>Gets the namespace of element.</summary>
+        /// <returns>the namespace</returns>
         public virtual PdfNamespace GetNamespace() {
             return null;
         }
 
+        /// <summary>Sets the namespace of element.</summary>
+        /// <param name="namespace">the namespace to be set</param>
+        /// <returns>
+        /// this
+        /// <see cref="AccessibilityProperties"/>
+        /// instance
+        /// </returns>
         public virtual AccessibilityProperties SetNamespace(PdfNamespace @namespace) {
             return this;
         }
 
+        /// <summary>Adds the reference to other tagged element.</summary>
+        /// <remarks>
+        /// Adds the reference to other tagged element.
+        /// <para />
+        /// For more information see
+        /// <see cref="iText.Kernel.Pdf.Tagging.PdfStructElem.AddRef(iText.Kernel.Pdf.Tagging.PdfStructElem)"/>.
+        /// </remarks>
+        /// <param name="treePointer">the reference to be set</param>
+        /// <returns>
+        /// this
+        /// <see cref="AccessibilityProperties"/>
+        /// instance
+        /// </returns>
         public virtual AccessibilityProperties AddRef(TagTreePointer treePointer) {
             return this;
         }
 
+        /// <summary>Gets the list of references to other tagged elements.</summary>
+        /// <remarks>
+        /// Gets the list of references to other tagged elements.
+        /// <para />
+        /// For more information see
+        /// <see cref="iText.Kernel.Pdf.Tagging.PdfStructElem.AddRef(iText.Kernel.Pdf.Tagging.PdfStructElem)"/>.
+        /// </remarks>
+        /// <returns>the list of references</returns>
         public virtual IList<TagTreePointer> GetRefsList() {
             return JavaCollectionsUtil.EmptyList<TagTreePointer>();
         }
 
+        /// <summary>Clears the list of references to other tagged elements.</summary>
+        /// <remarks>
+        /// Clears the list of references to other tagged elements.
+        /// <para />
+        /// For more information see
+        /// <see cref="iText.Kernel.Pdf.Tagging.PdfStructElem.AddRef(iText.Kernel.Pdf.Tagging.PdfStructElem)"/>.
+        /// </remarks>
+        /// <returns>
+        /// this
+        /// <see cref="AccessibilityProperties"/>
+        /// instance
+        /// </returns>
         public virtual AccessibilityProperties ClearRefs() {
             return this;
         }
 
+        /// <summary>Adds the attributes to the element.</summary>
+        /// <param name="attributes">the attributes to be added</param>
+        /// <returns>
+        /// this
+        /// <see cref="AccessibilityProperties"/>
+        /// instance
+        /// </returns>
         public virtual AccessibilityProperties AddAttributes(PdfStructureAttributes attributes) {
             return this;
         }
 
+        /// <summary>Adds the attributes to the element with specified index.</summary>
+        /// <remarks>
+        /// Adds the attributes to the element with specified index.
+        /// <para />
+        /// If an attribute with the same O and NS entries is specified more than once, the later (in array order)
+        /// entry shall take precedence. For more information see PDF Specification ISO-32000 section 14.7.6.
+        /// </remarks>
+        /// <param name="index">the attributes index</param>
+        /// <param name="attributes">the attributes to be added</param>
+        /// <returns>
+        /// this
+        /// <see cref="AccessibilityProperties"/>
+        /// instance
+        /// </returns>
         public virtual AccessibilityProperties AddAttributes(int index, PdfStructureAttributes attributes) {
             return this;
         }
 
+        /// <summary>Clears the list of attributes.</summary>
+        /// <returns>
+        /// this
+        /// <see cref="AccessibilityProperties"/>
+        /// instance
+        /// </returns>
         public virtual AccessibilityProperties ClearAttributes() {
             return this;
         }
 
+        /// <summary>Gets the attributes list.</summary>
+        /// <returns>the attributes list</returns>
         public virtual IList<PdfStructureAttributes> GetAttributesList() {
             return JavaCollectionsUtil.EmptyList<PdfStructureAttributes>();
         }
