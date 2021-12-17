d3118a1d7555e37cb49455d40a1912df95f1a7ca
iText Software <development@itextpdf.com>
2021-12-17 07:00:02 +0000

    [RELEASE] 7.2.1

--- itext.tests/itext.layout.tests/Properties/AssemblyInfo.cs
+++ itext.tests/itext.layout.tests/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("7.2.1.0")]
 [assembly: AssemblyFileVersion("7.2.1.0")]
-[assembly: AssemblyInformationalVersion("7.2.1-SNAPSHOT")]
+[assembly: AssemblyInformationalVersion("7.2.1")]
 
 #if !NETSTANDARD2_0
 [assembly: NUnit.Framework.Timeout(600000)]
