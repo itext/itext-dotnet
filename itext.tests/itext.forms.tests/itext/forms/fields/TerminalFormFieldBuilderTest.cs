11126ead28f34086bf0b849b43d0b20e9ab91b9b
iText Software <development@itextpdf.com>
2022-03-17 19:32:26 +0000

    Add missing copyright headers
    Autoported commit.
    Original commit hash: [1b798f3]

--- itext.tests/itext.forms.tests/itext/forms/fields/TerminalFormFieldBuilderTest.cs
+++ itext.tests/itext.forms.tests/itext/forms/fields/TerminalFormFieldBuilderTest.cs
@@ -1,3 +1,25 @@
+/*
+This file is part of the iText (R) project.
+Copyright (c) 1998-2022 iText Group NV
+Authors: iText Software.
+
+This program is offered under a commercial and under the AGPL license.
+For commercial licensing, contact us at https://itextpdf.com/sales.  For AGPL licensing, see below.
+
+AGPL licensing:
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 using System;
 using System.IO;
 using iText.Kernel.Geom;
@@ -44,12 +66,12 @@ namespace iText.Forms.Fields {
             TerminalFormFieldBuilderTest.TestBuilder builder = new TerminalFormFieldBuilderTest.TestBuilder(DUMMY_DOCUMENT
                 , DUMMY_NAME);
             builder.SetPage(5);
-            PdfFormField formField = new _PdfFormField_56(DUMMY_DOCUMENT);
+            PdfFormField formField = new _PdfFormField_78(DUMMY_DOCUMENT);
             builder.SetPageToField(formField);
         }
 
-        private sealed class _PdfFormField_56 : PdfFormField {
-            public _PdfFormField_56(PdfDocument baseArg1)
+        private sealed class _PdfFormField_78 : PdfFormField {
+            public _PdfFormField_78(PdfDocument baseArg1)
                 : base(baseArg1) {
             }
 
