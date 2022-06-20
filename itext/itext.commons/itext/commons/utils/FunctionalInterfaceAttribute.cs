d6d7da207f00a1195ca9a563214dd935f27a3aa8
glenn.volckaert <glenn.volckaert@itextpdf.com>
2022-06-20 16:04:51 +0200

    Add FunctionalInterfaceAttribute to allow cleaner java code
    DEVSIX-3538

---
+++ itext/itext.commons/itext/commons/utils/FunctionalInterfaceAttribute.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace iText.Commons.Utils
+{
+    /// <summary>
+    /// A mapping for java's FunctionalInterface attribute to allow clean code in java.
+    /// </summary>
+    public class FunctionalInterfaceAttribute : Attribute
+    {
+    }
+}
