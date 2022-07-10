b43cc4b10cbed0a3455f3c1f2a0aa6545e30a3da
Eugene Bochilo <eugene.bochilo@duallab.com>
2022-07-11 01:24:05 +0300

    Create new bouncy-castle modules in .Net
    DEVSIX-6729

---
+++ itext/itext.bouncy-castle-connector/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("iText.Bouncy-castle-connector")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("iText Group NV")]
+[assembly: AssemblyProduct("iText")]
+[assembly: AssemblyCopyright("Copyright (c) 1998-2022 iText Group NV")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("56eaf103-2032-43eb-a7dd-fbeea5c5cb9c")]
+
+[assembly: AssemblyVersion("8.0.0.0")]
+[assembly: AssemblyFileVersion("8.0.0.0")]
+[assembly: AssemblyInformationalVersion("8.0.0-SNAPSHOT")]
