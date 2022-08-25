8a93f21ff5e6f315097c9054524a9097fe904205
Angelina Pavlovets <angelina.pavlovets@duallab.com>
2022-08-25 12:14:34 +0300

    Remove unused bc-related methods from KernelExtensions
    DEVSIX-7021

--- itext/itext.kernel/KernelExtensions.cs
+++ itext/itext.kernel/KernelExtensions.cs
@@ -53,7 +53,6 @@ using System.Text;
 using System.Text.RegularExpressions;
 using iText.Commons.Utils;
 using iText.Commons.Utils.Collections;
-using Org.BouncyCastle.Crypto;
 
 internal static class KernelExtensions {
     public static String JSubstring(this String str, int beginIndex, int endIndex) {
@@ -388,25 +387,6 @@ internal static class KernelExtensions {
         return new Stack<T>(new Stack<T>(stack)); // create stack twice to retain the original order
     }
 
-    public static void Update(this IDigest dgst, byte[] input) {
-        dgst.Update(input, 0, input.Length);
-    }
-
-    public static void Update(this IDigest dgst, byte[] input, int offset, int len) {
-        dgst.BlockUpdate(input, offset, len);
-    }
-
-    public static byte[] Digest(this IDigest dgst) {
-        byte[] output = new byte[dgst.GetDigestSize()];
-        dgst.DoFinal(output, 0);
-        return output;
-    }
-
-    public static byte[] Digest(this IDigest dgst, byte[] input) {
-        dgst.Update(input);
-        return dgst.Digest();
-    }
-
     public static bool CanExecute(this FileInfo fileInfo)
     {
         return fileInfo.Exists;
@@ -416,17 +396,6 @@ internal static class KernelExtensions {
         T result = list.First.Value;
         list.RemoveFirst();
         return result;
-    }
-
-    /// <summary>
-    /// IMPORTANT: USE THIS METHOD CAREFULLY.
-    /// This method serves as replacement for the java method MessageDigest#digest(byte[] buf, int offset, int len).
-    /// However for now, we simply omit len parameter, because it doesn't affect anything for all current usages
-    /// (there are two of them at the moment of the method addition which are in StandardHandlerUsingAes256 class).
-    /// This may be not true for future possible usages, so be aware.
-    /// </summary>
-    public static void Digest(this IDigest dgst, byte[] buff, int offest, int len) {
-        dgst.DoFinal(buff, offest);
     }
 
 #if !NETSTANDARD2_0
