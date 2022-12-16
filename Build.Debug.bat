c1a24aad8dc7f2f67039747f552cd4206911b2d7
Piero Viano <piero.viano@gmail.com>
2022-12-16 13:32:30 +0200

    ''

--- Build.Debug.bat
+++ Build.Debug.bat
@@ -3,7 +3,7 @@ rmdir /s /q %userprofile%\.nuget\Packages\unofficialitext7
 del Packages\unofficialitext7.commons.*
 rmdir /s /q %userprofile%\.nuget\Packages\unofficialitext7.commons
 nuget.exe restore iTextCore.sln
-MSBuild.exe iTextCore.sln /property:Configuration=Debug
+MSBuild.exe iTextCore.sln -m /property:Configuration=Debug
 cd NuSpecs.Debug
 nuget pack -OutputDirectory ..\Packages unofficialitext7-commons.nuspec
 nuget pack -OutputDirectory ..\Packages unofficialitext7.nuspec
