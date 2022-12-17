95f4da05a54102b8934410a62639099cf776cf92
Piero Viano <piero.viano@gmail.com>
2022-12-17 07:23:49 +0200

    ''

--- Build.bat
+++ Build.bat
@@ -1,12 +1,12 @@
-del Packages\unofficialitext7.*
-rmdir /s /q %userprofile%\.nuget\Packages\unofficialitext7
-del Packages\unofficialitext7.commons.*
-rmdir /s /q %userprofile%\.nuget\Packages\unofficialitext7.commons
+del Packages\Net4x.itext7.*
+rmdir /s /q %userprofile%\.nuget\Packages\Net4x.itext7
+del Packages\Net4x.itext7.commons.*
+rmdir /s /q %userprofile%\.nuget\Packages\Net4x.itext7.commons
 nuget.exe restore iTextCore.sln
 MSBuild.exe iTextCore.sln -m /property:Configuration=Release
 cd NuSpecs
-nuget pack -OutputDirectory ..\Packages unofficialitext7-commons.nuspec
-nuget pack -OutputDirectory ..\Packages unofficialitext7.nuspec
+nuget pack -OutputDirectory ..\Packages Net4x.itext7-commons.nuspec
+nuget pack -OutputDirectory ..\Packages Net4x.itext7.nuspec
 cd ..
 git add -A
 git commit -a --allow-empty-message -m ''
