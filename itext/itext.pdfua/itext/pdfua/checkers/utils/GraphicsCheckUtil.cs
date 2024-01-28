e79cc7c5da3d47197e1789cd011f0b70365b9737
iText Software <development@itextpdf.com>
2024-01-28 10:01:50 +0000

    Add missing copyright headers
    Autoported commit.
    Original commit hash: [2ae9860e2]

--- itext/itext.pdfua/itext/pdfua/checkers/utils/GraphicsCheckUtil.cs
+++ itext/itext.pdfua/itext/pdfua/checkers/utils/GraphicsCheckUtil.cs
@@ -1,3 +1,25 @@
+/*
+This file is part of the iText (R) project.
+Copyright (c) 1998-2024 Apryse Group NV
+Authors: Apryse Software.
+
+This program is offered under a commercial and under the AGPL license.
+For commercial licensing, contact us at https://itextpdf.com/sales.  For AGPL licensing, see below.
+
+AGPL licensing:
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 using System;
 using iText.Kernel.Pdf;
 using iText.Kernel.Pdf.Tagging;
@@ -42,11 +64,11 @@ namespace iText.Pdfua.Checkers.Utils {
         /// description or actual text
         /// </returns>
         public static ITagTreeIteratorHandler CreateFigureTagHandler() {
-            return new _ITagTreeIteratorHandler_57();
+            return new _ITagTreeIteratorHandler_79();
         }
 
-        private sealed class _ITagTreeIteratorHandler_57 : ITagTreeIteratorHandler {
-            public _ITagTreeIteratorHandler_57() {
+        private sealed class _ITagTreeIteratorHandler_79 : ITagTreeIteratorHandler {
+            public _ITagTreeIteratorHandler_79() {
             }
 
             public void NextElement(IStructureNode elem) {
