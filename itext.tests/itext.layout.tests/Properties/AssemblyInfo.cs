78f1884b6d6115f016c5cade514d9bf4b3e0b2da
Apryse Software <development@apryse.com>
2024-04-18 15:24:48 +0000

    [RELEASE] 8.0.4

--- itext.tests/itext.layout.tests/Properties/AssemblyInfo.cs
+++ itext.tests/itext.layout.tests/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("8.0.4.0")]
 [assembly: AssemblyFileVersion("8.0.4.0")]
-[assembly: AssemblyInformationalVersion("8.0.4-SNAPSHOT")]
+[assembly: AssemblyInformationalVersion("8.0.4")]
 
 #if !NETSTANDARD2_0
 [assembly: NUnit.Framework.Timeout(600000)]
