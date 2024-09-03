70175c9e4f90ee64435bea8a378c7c28274d8f3e
Guust Ysebie <guust.ysebie@apryse.com>
2024-09-03 22:19:00 +0200

    Register default kernel di implementation
    DEVSIX-8579

--- itext/itext.commons/itext/commons/utils/DIContainerConfigurations.cs
+++ itext/itext.commons/itext/commons/utils/DIContainerConfigurations.cs
@@ -26,9 +26,10 @@ using System.Reflection;
 
 namespace iText.Commons.Utils {
     public class DIContainerConfigurations {
-
-        private static readonly Dictionary<String, String> DEFAULT_CONFIGURATIONS = new Dictionary<String, String>() {
-            { "iText.Forms", "iText.Forms.Util.RegisterDefaultDiContainer" }
+        private static readonly Dictionary<String, String> DEFAULT_CONFIGURATIONS = new Dictionary<String, String>()
+        {
+            { "iText.Forms", "iText.Forms.Util.RegisterDefaultDiContainer" },
+            { "iText.Kernel","iText.Kernel.Utils.RegisterDefaultDiContainer" }
         };
 
         public static void LoadDefaultConfigurations() {
