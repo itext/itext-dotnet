c9e6f84e86359331c521a428542f5816a62fd1b4
Apryse Software <development@apryse.com>
2025-02-05 11:36:23 +0000

    [RELEASE] 9.1.0

--- itext/itext.io/Properties/AssemblyInfo.cs
+++ itext/itext.io/Properties/AssemblyInfo.cs
@@ -37,4 +37,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("9.1.0.0")]
 [assembly: AssemblyFileVersion("9.1.0.0")]
-[assembly: AssemblyInformationalVersion("9.1.0-SNAPSHOT")]
+[assembly: AssemblyInformationalVersion("9.1.0")]
