4e2c2294aae575a484def4ec1ad8a2c6c4664c7a
iText Software <development@itextpdf.com>
2025-06-12 11:04:30 +0000

    Add missing copyright headers
    Autoported commit.
    Original commit hash: [0927387ba]

--- itext.tests/itext.sign.tests/itext/signatures/lotl/EuropeanListOfTrustedListFetcherTest.cs
+++ itext.tests/itext.sign.tests/itext/signatures/lotl/EuropeanListOfTrustedListFetcherTest.cs
@@ -1,3 +1,25 @@
+/*
+This file is part of the iText (R) project.
+Copyright (c) 1998-2025 Apryse Group NV
+Authors: Apryse Software.
+
+This program is offered under a commercial and under the AGPL license.
+For commercial licensing, contact us at https://itextpdf.com/sales.  For AGPL licensing, see below.
+
+AGPL licensing:
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 using System;
 using System.IO;
 using iText.Commons.Exceptions;
@@ -37,13 +59,13 @@ namespace iText.Signatures.Lotl {
 
         [NUnit.Framework.Test]
         public virtual void DummmyRetrieverCausesException() {
-            EuropeanListOfTrustedListFetcher fetcher = new EuropeanListOfTrustedListFetcher(new _IResourceRetriever_57
+            EuropeanListOfTrustedListFetcher fetcher = new EuropeanListOfTrustedListFetcher(new _IResourceRetriever_79
                 ());
             NUnit.Framework.Assert.Catch(typeof(ITextException), () => fetcher.GetLotlData());
         }
 
-        private sealed class _IResourceRetriever_57 : IResourceRetriever {
-            public _IResourceRetriever_57() {
+        private sealed class _IResourceRetriever_79 : IResourceRetriever {
+            public _IResourceRetriever_79() {
             }
 
             public Stream GetInputStreamByUrl(Uri url) {
