c2a53edeb1b1de7de84cde5592b397f551646de6
iText Software <development@itextpdf.com>
2025-12-10 05:29:28 +0000

    Add missing copyright headers
    Autoported commit.
    Original commit hash: [d9a0d85b1]

--- itext.tests/itext.sign.tests/itext/signatures/validation/report/pades/CertificateEventsFiredTest.cs
+++ itext.tests/itext.sign.tests/itext/signatures/validation/report/pades/CertificateEventsFiredTest.cs
@@ -1,3 +1,25 @@
+/*
+This file is part of the iText (R) project.
+Copyright (c) 1998-2025 Apryse Group NV
+Authors: Apryse Software.
+
+This program is offered under a commercial and under the AGPL license.
+For commercial licensing, contact us at https://itextpdf.com/sales.  For AGPL licensing, see below.
+
+AGPL licensing:
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 using System;
 using iText.Commons.Bouncycastle.Cert;
 using iText.Commons.Utils;
@@ -40,11 +62,11 @@ namespace iText.Signatures.Validation.Report.Pades {
             caCertificate = (IX509Certificate)PemFileHelper.ReadFirstChain(CERTS_SRC + "signCertRsa01.pem")[1];
             certificateRetriever = new IssuingCertificateRetriever();
             builder.WithIssuingCertificateRetrieverFactory(() => certificateRetriever);
-            builder.WithRevocationDataValidatorFactory(() => new _RevocationDataValidator_50(builder));
+            builder.WithRevocationDataValidatorFactory(() => new _RevocationDataValidator_72(builder));
         }
 
-        private sealed class _RevocationDataValidator_50 : RevocationDataValidator {
-            public _RevocationDataValidator_50(ValidatorChainBuilder baseArg1)
+        private sealed class _RevocationDataValidator_72 : RevocationDataValidator {
+            public _RevocationDataValidator_72(ValidatorChainBuilder baseArg1)
                 : base(baseArg1) {
             }
 
