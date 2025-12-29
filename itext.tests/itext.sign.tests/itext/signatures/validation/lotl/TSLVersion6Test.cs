75289b74cf55c27d8404e8a602b7f7c6efedfae2
iText Software <development@itextpdf.com>
2025-12-29 17:57:27 +0000

    Add missing copyright headers
    Autoported commit.
    Original commit hash: [02ccfee92]

--- itext.tests/itext.sign.tests/itext/signatures/validation/lotl/TSLVersion6Test.cs
+++ itext.tests/itext.sign.tests/itext/signatures/validation/lotl/TSLVersion6Test.cs
@@ -1,3 +1,25 @@
+/*
+This file is part of the iText (R) project.
+Copyright (c) 1998-2025 Apryse Group NV
+Authors: Apryse Software.
+
+This program is offered under a commercial and under the AGPL license.
+For commercial licensing, contact us at https://itextpdf.com/sales.  For AGPL licensing, see below.
+
+AGPL licensing:
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -43,15 +65,15 @@ namespace iText.Signatures.Validation.Lotl {
             LotlService service = new LotlService(new LotlFetchingProperties(new ThrowExceptionOnFailingCountryData())
                 );
             service.WithCustomResourceRetriever(new FromDiskResourceRetriever(SOURCE_FOLDER_LOTL_FILES));
-            EuropeanTrustedListConfigurationFactory.SetFactory(() => new _EuropeanTrustedListConfigurationFactory_65()
+            EuropeanTrustedListConfigurationFactory.SetFactory(() => new _EuropeanTrustedListConfigurationFactory_87()
                 );
             service.WithLotlValidator(() => new LotlValidator(service));
             LotlService.GLOBAL_SERVICE = service;
             service.InitializeCache();
         }
 
-        private sealed class _EuropeanTrustedListConfigurationFactory_65 : EuropeanTrustedListConfigurationFactory {
-            public _EuropeanTrustedListConfigurationFactory_65() {
+        private sealed class _EuropeanTrustedListConfigurationFactory_87 : EuropeanTrustedListConfigurationFactory {
+            public _EuropeanTrustedListConfigurationFactory_87() {
             }
 
             public override String GetTrustedListUri() {
