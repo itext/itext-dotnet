c377fc87fd3e1fac2932a1a6d2776f3049430c3c
iText Software <development@itextpdf.com>
2026-02-16 08:42:01 +0000

    Add test method with random retry
    DEVSIX-9296
    
    Autoported commit.
    Original commit hash: [ec2e4cbab]
    Manual files:
    pdftest/src/main/java/com/itextpdf/test/AssertUtil.java
    pdftest/src/test/java/com/itextpdf/test/AssertUtilTest.java

--- itext/itext.commons/itext/commons/utils/TimerUtil.cs
+++ itext/itext.commons/itext/commons/utils/TimerUtil.cs
@@ -28,7 +28,7 @@ namespace iText.Commons.Utils {
     public class TimerUtil {
         /// <summary>Creates a new Timer instance.</summary>
         /// <returns>a new Timer instance</returns>
-        public static Timer NewTimerWithRecurringTask(Action task, long delay, long period) {
+        public static Timer NewTimerWithRecurringTask(System.Action task, long delay, long period) {
             Timer timer = new Timer((state) => { task(); }, null, delay, period);
             return timer;
         }
--- itext/itext.pdftest/itext/test/AssertUtil.cs
+++ itext/itext.pdftest/itext/test/AssertUtil.cs
@@ -1,34 +1,34 @@
 /*
-    This file is part of the iText (R) project.
+This file is part of the iText (R) project.
 Copyright (c) 1998-2026 Apryse Group NV
-    Authors: Apryse Software.
+Authors: Apryse Software.
 
-    This program is offered under a commercial and under the AGPL license.
-    For commercial licensing, contact us at https://itextpdf.com/sales.  For AGPL licensing, see below.
+This program is offered under a commercial and under the AGPL license.
+For commercial licensing, contact us at https://itextpdf.com/sales.  For AGPL licensing, see below.
 
-    AGPL licensing:
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+AGPL licensing:
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
 
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 using System;
 using System.Threading;
 using NUnit.Framework;
 
-namespace iText.Test 
-{
-
-    public class AssertUtil 
-    {
+namespace iText.Test {
+    /// <summary>Utilities class for assertion operation.</summary>
+    public class AssertUtil {
+        private AssertUtil() {
+        }
 
         public static void AssertThrows(Type expectedThrowable, TestDelegate runnable)
         {
@@ -40,26 +40,41 @@ namespace iText.Test
             Assert.That(runnable, Throws.InstanceOf(expectedThrowable).With.Message.EqualTo(message));
         }
 
-        /// <summary>
-        /// Assert that the assertion passed within the timeout.
-        /// </summary>
-        /// <param name="assertion">Callback to the actuals asserts to be safeguarded.</param>
-        /// <param name="timeout">The maximum tilme it can take before passing the assertions.</param>
+        /// <summary>Assert that the assertion passed within the timeout</summary>
+        /// <param name="assertion">callback to the actual asserts to be safeguarded</param>
+        /// <param name="timeout">the maximum time it can take before passing the assertions</param>
         public static void AssertPassedWithinTimeout(Action assertion, TimeSpan timeout)
         {
             // Pass 1 millis sleepTime to force thread yield
             AssertPassedWithinTimeout(assertion, timeout, TimeSpan.FromMilliseconds(1));
         }
 
-        /// <summary>
-        /// Assert that the assertion passed within the timeout.
-        /// </summary>
-        /// <param name="assertion">Callback to the actuals asserts to be safeguarded.</param>
-        /// <param name="timeout">The maximum tilme it can take before passing the assertions.</param>
-        /// <param name="sleepTime">The time to sleep between polls.</param>
-        public static void AssertPassedWithinTimeout(Action assertion, TimeSpan timeout, TimeSpan sleepTime)
-        {
-            int sleepTimeInMillies = (int)sleepTime.TotalMilliseconds;
+        /// <summary>Assert that the assertion passed within the timeout</summary>
+        /// <param name="assertion">callback to the actual asserts to be safeguarded</param>
+        /// <param name="timeout">the maximum time it can take before passing the assertions</param>
+        /// <param name="sleepTime">the time to sleep between polls</param>
+        public static void AssertPassedWithinTimeout(Action assertion, TimeSpan timeout, TimeSpan
+             sleepTime) {
+            AssertPassedWithinTimeout(assertion, timeout, () => Convert.ToInt64(Math.Round(sleepTime.TotalMilliseconds)));
+        }
+
+        /// <summary>Assert that the assertion passed within the random timeout.</summary>
+        /// <remarks>
+        /// Assert that the assertion passed within the random timeout.
+        /// <para />
+        /// This method may be useful when your tests fetch some stateful resources concurrently. It allows to de-synchronize
+        /// access from different clients. It's exponential backoff with jitter but without 'exponential' part. Because
+        /// it does not really care if tests become longer, the main goal is to pass the tests.
+        /// </remarks>
+        /// <param name="assertion">callback to the actual asserts to be safeguarded</param>
+        /// <param name="timeout">the maximum time it can take before passing the assertions</param>
+        /// <param name="maxSleepTime">the maximum time to sleep between polls</param>
+        public static void AssertPassedWithinRandomTimeout(Action assertion, TimeSpan timeout
+            , TimeSpan maxSleepTime) {
+            AssertPassedWithinTimeout(assertion, timeout, () => Convert.ToInt64(Math.Round(maxSleepTime.TotalMilliseconds * new Random().NextDouble())));
+        }
+
+        private static void AssertPassedWithinTimeout(Action assertion, TimeSpan timeout, Func<long> timeoutCalc) {
             var watch = System.Diagnostics.Stopwatch.StartNew();
 
             var elapsedMs = watch.ElapsedMilliseconds;
@@ -77,7 +92,8 @@ namespace iText.Test
                     {
                         throw e;
                     }
-                    Thread.Sleep(sleepTimeInMillies);
+                    int a = (int) timeoutCalc();
+                    Thread.Sleep((int) timeoutCalc());
                     //ignore assertion failure if timeout not spent.                                                                                       
                 }
 
