74b1b223e16db9890935ffe956171af729ace881
Apryse Software <development@apryse.com>
2026-03-26 11:05:19 +0000

    [RELEASE] 9.6.0

--- itext.tests/itext.svg.tests/Properties/AssemblyInfo.cs
+++ itext.tests/itext.svg.tests/Properties/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using NUnit.Framework;
 
 [assembly: AssemblyVersion("9.6.0.0")]
 [assembly: AssemblyFileVersion("9.6.0.0")]
-[assembly: AssemblyInformationalVersion("9.6.0-SNAPSHOT")]
+[assembly: AssemblyInformationalVersion("9.6.0")]
 
 [assembly: Parallelizable(ParallelScope.ContextMask)]
 
