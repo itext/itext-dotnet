d82466e010872600f37889d7db3b591319bb6cb8
Apryse <development@apryse.com>
2026-06-30 21:41:52 +0000

    Add missing copyright headers
    Autoported commit.
    Original commit hash: [a07fcb841]
    Manual files:
    io/src/main/java/com/itextpdf/io/image/WebPLogMessageConstant.java
    layout/src/main/java/com/itextpdf/layout/renderer/typography/ScriptInfoData.java

--- itext.tests/itext.layout.tests/itext/layout/InfiniteLoopTest.cs
+++ itext.tests/itext.layout.tests/itext/layout/InfiniteLoopTest.cs
@@ -1,3 +1,25 @@
+/*
+This file is part of the iText (R) project.
+Copyright (c) 1998-2026 Apryse Group NV
+Authors: Apryse Software.
+
+This program is offered under a commercial and under the AGPL license.
+For commercial licensing, contact us at https://itextpdf.com/sales.  For AGPL licensing, see below.
+
+AGPL licensing:
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 using System;
 using iText.IO.Image;
 using iText.Kernel.Exceptions;
@@ -40,14 +62,14 @@ namespace iText.Layout {
                 (10_000));
             using (PdfDocument pdfDocument = new PdfDocument(new PdfWriter(outFileName), documentProperties)) {
                 using (Document document = new Document(pdfDocument)) {
-                    Paragraph paragraph = new _Paragraph_55();
+                    Paragraph paragraph = new _Paragraph_77();
                     NUnit.Framework.Assert.Catch(typeof(PdfException), () => document.Add(paragraph));
                 }
             }
         }
 
-        private sealed class _Paragraph_55 : Paragraph {
-            public _Paragraph_55() {
+        private sealed class _Paragraph_77 : Paragraph {
+            public _Paragraph_77() {
             }
 
             protected internal override IRenderer MakeNewRenderer() {
