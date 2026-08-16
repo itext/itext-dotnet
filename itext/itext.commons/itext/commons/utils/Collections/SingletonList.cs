4b0bfac62403cfdfe9c26a924b642a6856d42d8c
Eugene Bochilo <eugene.bochilo@apryse.com>
2026-08-16 09:22:27 +0300

    Change SingletonList so that it extends IList
    DEVSIX-10118

--- itext/itext.commons/itext/commons/utils/Collections/SingletonList.cs
+++ itext/itext.commons/itext/commons/utils/Collections/SingletonList.cs
@@ -27,7 +27,7 @@ using System.Collections.Generic;
 namespace iText.Commons.Utils.Collections
 {
     //\cond DO_NOT_DOCUMENT 
-    internal class SingletonList<T> : IList<T> {
+    internal class SingletonList<T> : IList<T>, IList {
         private readonly T element;
 
         public SingletonList(T obj) {
@@ -46,19 +46,39 @@ namespace iText.Commons.Utils.Collections
             throw new NotSupportedException("Collection is read-only.");
         }
 
+        public int Add(object value)
+        {
+            throw new NotSupportedException("Collection is read-only.");
+        }
+
         public void Clear() {
             throw new NotSupportedException("Collection is read-only.");
         }
 
+        public bool Contains(object value)
+        {
+            return (element == null && value == null || element != null && element.Equals(value));
+        }
+        
         public bool Contains(T item) {
-            return (element == null && item == null || element != null && element.Equals(item));
+            return Contains((object)item);
         }
 
         public void CopyTo(T[] array, int arrayIndex) {
             array[arrayIndex] = element;
         }
+        
+        public void CopyTo(Array array, int index)
+        {
+            array.SetValue(element, index);
+        }
 
         public bool Remove(T item) {
+            throw new NotSupportedException("Collection is read-only.");
+        }
+        
+        public void Remove(object value)
+        {
             throw new NotSupportedException("Collection is read-only.");
         }
 
@@ -66,23 +86,59 @@ namespace iText.Commons.Utils.Collections
             get { return 1; }
         }
 
+        public object SyncRoot
+        {
+            get { return this; }
+        }
+        public bool IsSynchronized
+        {
+            get { return true; }
+        }
+
         public bool IsReadOnly {
             get { return true; }
         }
 
-        public int IndexOf(T item) {
-            if (Contains(item)) {
-                return 0;
-            }
-            return -1;
+        public bool IsFixedSize
+        {
+            get { return true; }
+        }
+
+        public int IndexOf(T item)
+        {
+            return IndexOf((object) item);
+        }
+        
+        public int IndexOf(object value)
+        {
+            return Contains(value) ? 0 : -1;
+        }
+
+        public void Insert(int index, object value)
+        {
+            throw new NotSupportedException("Collection is read-only.");
         }
 
         public void Insert(int index, T item) {
-            throw new InvalidOperationException();
+            throw new NotSupportedException("Collection is read-only.");
         }
 
         public void RemoveAt(int index) {
             throw new NotSupportedException("Collection is read-only.");
+        }
+
+        object IList.this[int index]
+        {
+            get
+            {
+                if (index != 0)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                return element;
+            }
+            set { throw new NotSupportedException("Collection is read-only."); }
         }
 
         public T this[int index] {
